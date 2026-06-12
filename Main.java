96b5732d84b2410008d5007f67113b84cd5f507b
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-12 11:17:04 +0530

    Updated Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Ganesh!");
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello Ganesh!");
+//     }
+// }
+
+//interface
+interface Vehicle{
+    void start();
+}
+class Car implements Vehicle{
+    @Override
+    public void start(){
+        System.out.println("Car Started");
+    }
+}
+public class Main{
+    public static void main(String[] args){
+        Car c1=new Car();
+        c1.start();
     }
 }
