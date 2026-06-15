56af52e4bfd89d5a3206e6a20c9e8fa706293930
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-15 14:57:19 +0530

    Day08 All List

---
+++ Day08/Main.java
@@ -0,0 +1,121 @@
+package Day08;
+// import java.util.*;
+// public class Main {
+//     public static void main(String[] args){
+//         HashMap<Integer,String> students=new HashMap<>();
+//         students.put(101,"Ganesh");
+//          students.put(102,"Java");
+//           students.put(102,"Spring Boot");
+
+// import java.util.LinkedHashMap;
+// import java.util.TreeMap;
+// import java.util.*;
+
+//           System.out.println(students);
+//     }
+// }
+// public class Main {
+
+//     public static void main(String[] args){
+//         TreeMap<Integer, String> students=new TreeMap<>();
+//         students.put(103,"Python");
+//         students.put(101,"Ganesh");
+//         students.put(102,"Java");
+//         System.out.println(students);
+//     }
+// }
+
+// public class Main {
+
+//     public static void main(String[] args){
+//         LinkedHashMap<Integer, String> students=new LinkedHashMap<>();
+//         students.put(103,"Python");
+//         students.put(101,"Ganesh");
+//         students.put(102,"Java");
+//         System.err.println(students);
+//     }
+// }
+
+// public class Main {
+
+//     public static void main(String[] args){
+//         ArrayList<String> names=new ArrayList<>();
+//         names.add("Ganesh");
+//         names.add("Java ");
+//         names.add("Python");
+
+//         Iterator<String> it=names.iterator();
+//         while (it.hasNext()) {
+//             System.out.println(it.next());
+//         }
+//     }
+
+// }
+// public class Main {
+
+//     public static void main(String[] args){
+//         ArrayList<String> names=new ArrayList<>();
+//         names.add("Python");
+//         names.add("Java");
+//         names.add("C");
+//         Collections.sort(names);
+//         System.out.println("Sorted: "+names);
+//         Collections.reverse(names);
+//         System.out.println("Reversed: "+names);
+//     }
+// }
+// public class Main {
+
+//     public static void main(String[] args){
+//         ArrayList<Integer> numbers=new ArrayList<>();
+//         numbers.add(10);
+//         numbers.add(50);
+//         numbers.add(20);
+//         numbers.add(5);
+//         System.out.println("Max: "+Collections.max(numbers));
+//         System.out.println("Max: "+Collections.min(numbers));
+//     }
+// }
+/*
+Collections Cheat Sheet
+ArrayList
+add()
+get()
+set()
+remove()
+size()
+LinkedList
+addFirst()
+addLast()
+HashSet
+No duplicates
+No order guarantee
+TreeSet
+No duplicates
+Automatically sorted
+LinkedHashSet
+No duplicates
+Maintains insertion order
+HashMap
+put()
+get()
+
+Stores:
+
+Key -> Value
+TreeMap
+Sorted by key
+LinkedHashMap
+Maintains insertion order
+Iterator
+Iterator<String> it = list.iterator();
+
+while(it.hasNext()){
+    System.out.println(it.next());
+}
+Algorithms
+Collections.sort(list);
+Collections.reverse(list);
+Collections.max(list);
+Collections.min(list);
+*/
