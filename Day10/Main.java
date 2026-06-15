4d7f47732581ab844c4548784640d8b8316e1637
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-15 17:23:46 +0530

    Day10

---
+++ Day10/Main.java
@@ -0,0 +1,37 @@
+package Day10;
+// import java.util.*;
+// interface Greeting{
+//     void sayHello();
+// }
+
+// public class Main {
+//     public static void main(String[] args){
+//         Greeting g=()->{
+//             System.out.println("Hello Ganesh");
+//         };
+//         g.sayHello();
+//     }
+// }
+// interface Addition{
+//     int add(int a, int b);
+// }
+// public class Main {
+
+//     public static void main(String[] args){
+//         Addition obj=(a,b)->a+b;
+//         System.out.println(obj.add(10, 20));
+//     }
+// }
+
+// public class Main {
+
+//     public static void main(String[] args){
+//         ArrayList<Integer> numbers=new ArrayList<>();
+//         numbers.add(50);
+//         numbers.add(10);
+//         numbers.add(30);
+//         numbers.add(20);
+//         Collections.sort(numbers, Collections.reverseOrder());
+//         System.out.println(numbers);
+//     }
+// }
