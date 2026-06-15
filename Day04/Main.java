49d024fe61652d3149fdf4aa880bcf5a96eb9b34
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-15 10:57:53 +0530

    Day04 error handling

---
+++ Day04/Main.java
@@ -0,0 +1,13 @@
+package Day04;
+
+public class Main {
+    public static void main(String[] args){
+        try{
+            int[] arr={1,2,3};
+            System.out.println(arr[5]);
+        }catch(Exception e){
+            System.out. println("Array Error");
+        }
+        System.out.println("Program Continues");
+    }
+}
