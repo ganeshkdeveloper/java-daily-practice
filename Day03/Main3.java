3510e0623c740a7e4fc05159d406444ca1428a5c
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-15 10:04:52 +0530

    Day03 of java

---
+++ Day03/Main3.java
@@ -0,0 +1,98 @@
+package Day03;
+// import java.util.*;
+import java.time.LocalDate;
+// enum Day{
+//     MONDAY,
+//     TUESDAY,
+//     WEDNESDAY
+// }
+// public class Main3 {
+//     public static void main(String[] args){
+// Day today=Day.WEDNESDAY;
+//     System.out.println(today);
+//     }
+// import java.time.LocalTime;
+// import java.time.LocalDateTime;
+    
+// }
+
+// public class Main3 {
+
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter your name: ");
+//         String name=sc.nextLine();
+//         System.out.println(name);
+//     }
+// }
+// public class Main3 {
+
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter Your Age: ");
+//         int age=sc.nextInt();
+//         System.out.println("Your Age is: "+age);
+//     }
+// }
+// public class Main3 {
+
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter your name: ");
+//         String name=sc.nextLine();
+//         System.out.println("Hello "+name);
+//     }
+// }
+// public class Main3 {
+
+//     public static void main(String[] args){
+//         LocalDate today=LocalDate.now();
+//         System.out.println(today);
+//         LocalTime time=LocalTime.now();
+//         System.out.println(time);
+//         LocalDateTime dt= LocalDateTime.now();
+//         System.out.println(dt);
+//     }
+// }
+// public class Main3 {
+
+//     public static void main(String[] args){
+//         LocalDate today=LocalDate.now();
+//         System.out.println(today);
+//     }
+// }
+
+// class Person {
+// String name="Ganesh";
+//     class Details{
+//         void show(){
+//             System.out.println(name);
+//         }
+//     }
+// }
+// public class Main3 {
+
+//   public static void main(String[] args){
+//       Person p=new Person();
+//     Person.Details d=p.new Details();
+//     d.show();
+//   }
+    
+// }
+// class Vehicle{
+//     void start(){
+//         System.out.println("Vehicle Started");
+//     }
+// }
+// public class Main3 {
+
+//     public static void main(String[] args){
+//         Vehicle v=new Vehicle(){
+//             @Override
+//             void start(){
+//                 System.out.println("Car started");
+//             }
+//         };
+//         v.start();
+//     }
+// }
