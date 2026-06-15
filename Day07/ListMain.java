4f143e46aa7f5df6eeca5c536b545da58c68adac
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-15 14:13:58 +0530

    Day07

---
+++ Day07/ListMain.java
@@ -0,0 +1,150 @@
+package Day07;
+// import java.util.List;
+// public class ListMain {
+//     public static void main(String[] args){
+//         List<String> names=new ArrayList<>();
+//         names.add("Ganesh");
+//         names.add("Java");
+//         names.add("Python");
+//         System.out.println(names);
+//     }
+// }
+// public class ListMain {
+// import java.util.*;
+
+//     public static void main(String[] args){
+//         ArrayList<String> names=new ArrayList<>();
+//         names.add("Ganesh");
+//         names.add("Java");
+//         names.add("Python");
+//         System.out.println(names);
+//         names.remove(1);
+//         System.out.println(names);
+//         System.out.println(names.size());
+//         System.out.println(names.get(1));
+//     }
+// }
+// public class ListMain {
+
+//      public static void main(String[] args){
+//         ArrayList<String> names=new ArrayList<>();
+//         names.add("Ganesh");
+//         names.add("Java");
+//         names.add("Python");
+//         for(String name:names){
+//         System.out.println(name);    
+//         }
+        
+//      }
+// }
+/*ArrayList Topics Completed
+
+✅ Create ArrayList
+
+ArrayList<String> names = new ArrayList<>();
+
+✅ Add Elements
+
+names.add("Java");
+
+✅ Get Element
+
+names.get(1);
+
+✅ Remove Element
+
+names.remove(1);
+
+✅ Size
+
+names.size();
+
+✅ Update Element
+
+names.set(1, "Spring Boot");
+
+✅ Loop Through ArrayList
+
+for(String name : names) */
+
+
+// public class ListMain {
+
+//     public static void main(String[] args){
+//         LinkedList<String> names=new LinkedList<>();
+//         names.add("Ganesh");
+//         names.add("Java");
+//         names.add("Python");
+//         System.out.println(names);
+//     }
+// }
+// public class ListMain {
+
+//     public static void main(String[] args){
+//         LinkedList<String> names=new LinkedList<>();
+//         names.add("Java");
+//         names.add("Python");
+//         names.addFirst("Ganesh");
+//         names.addLast("Spring Boot");
+//         System.out.println(names);
+//     }
+// }
+/*
+LinkedList Methods Learned
+
+✅ add()
+
+names.add("Java");
+
+✅ addFirst()
+
+names.addFirst("Ganesh");
+
+✅ addLast()
+
+names.addLast("Spring Boot");
+*/
+// public class ListMain {
+
+//     public static void main(String[] args){
+//         ArrayList<String> names=new ArrayList<>();
+//         names.add("Python");
+//         names.add("Java");
+//         names.add("Spring Boot");
+//         names.add("C");
+//         Collections.sort(names);
+//         System.out.println(names);
+//     }
+// }
+// public class ListMain {
+
+//     public static void main(String[] args){
+//         HashSet<String> languages =new HashSet<>();
+//         languages.add("Java");
+//         languages.add("Python");
+//         languages.add("Java");
+//         System.out.println(languages);
+//     }
+// }
+// public class ListMain {
+
+//     public static void main(String[] args){
+//         TreeSet<String> languages=new TreeSet<>();
+//         languages.add("Python");
+//         languages.add("Java");
+//         languages.add("Spring Boot");
+//         languages.add("C"); 
+//         System.out.println(languages);
+//     }
+// }
+// public class ListMain {
+
+//     public static void main(String[] args){
+//         LinkedHashSet<String> languages=new LinkedHashSet<>();
+//         languages.add("Python");
+//         languages.add("Java");
+//         languages.add("Spring Boot");
+//         languages.add("C");
+//         System.out.println(languages);
+//     }
+// }
