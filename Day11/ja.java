229b5ac19a7f5c16f75086e34687f831ea6fd11d
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-16 09:55:57 +0530

    commits

--- Day11/ja.java
+++ Day11/ja.java
@@ -1,17 +1,32 @@
 package Day11;
 
-public class ja {
-    static int findFirstNegative(int[] arr){
+// public class ja {
+//     static int findFirstNegative(int[] arr){
     
-        for(int num:arr){
+//         for(int num:arr){
+//             if(num<0){
+//                 return num;
+//             }
+//         }
+//         return -1;
+//     }
+//     public static void main(String[] args){
+//         int[] arr={5, 8, -3, 10, -7};
+//         System.out.println(findFirstNegative(arr));
+//     }
+// }
+public class ja {
+    static int findLastNegative(int[] arr){
+           int lastNegative = -1;
+           for(int num:arr){
             if(num<0){
-                return num;
+                lastNegative=num;
             }
-        }
-        return -1;
+           }
+           return lastNegative;
     }
     public static void main(String[] args){
-        int[] arr={5, 8, -3, 10, -7};
-        System.out.println(findFirstNegative(arr));
+        int[] arr = {5, 8, -3, 10, -7};
+        System.out.println(findLastNegative(arr));
     }
 }
