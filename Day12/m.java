73dcadc7846c13fa5e1d566d8c2821913aa3067e
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-16 10:11:29 +0530

    today16626

---
+++ Day12/m.java
@@ -0,0 +1,12 @@
+package Day12;
+
+public class m {
+    public static void main(String[] args){
+    String  name = "Ganesh";
+       int  age = 26;
+      double  salary = 25000.50;
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Salary: "+salary);
+    }
+}
