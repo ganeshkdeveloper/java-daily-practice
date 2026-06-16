72ec0011c308d8337835f52b73971408a8231f66
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-16 09:31:02 +0530

    new commit

--- Day11/p.java
+++ Day11/p.java
@@ -1,18 +1,34 @@
 package Day11;
 
-public class p {
-    static int countNegativeNumbers(int[] arr){
-       int count=0;
-        for(int num:arr){
-            if(num<0){
-                count++;
-            }
-        }
-        return count;
-    }
-    public static void main(String[] args){
-        int[] arr = {-5, 3, 0, 8, -2, 10};
-        System.out.println(countNegativeNumbers(arr));
-    }
+// public class p {
+//     static int countNegativeNumbers(int[] arr){
+//        int count=0;
+//         for(int num:arr){
+//             if(num<0){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+//     public static void main(String[] args){
+//         int[] arr = {-5, 3, 0, 8, -2, 10};
+//         System.out.println(countNegativeNumbers(arr));
+//     }
     
-}
+// }
+
+// public class p {
+//     static int countZeros(int[] arr){
+//         int count=0;
+//         for(int num:arr){
+//             if(num==0){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+//     public static void main(String[] args){
+//         int[] arr={0, 5, 0, 8, -2, 0};
+//         System.out.println(countZeros(arr));
+//     }
+// }
