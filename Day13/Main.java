c1b04427fe07b46ab18104f4aab1be1fbfdead4e
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-16 12:59:15 +0530

    day13

---
+++ Day13/Main.java
@@ -0,0 +1,56 @@
+package Day13;
+
+// public class main {
+//     static int square(int n){
+//         return n*n;
+//     }
+//     public static void main(String[] args) {
+//         System.out.println(square(5));
+//     }
+// }
+
+// public class main {
+
+//     static void greet(String name){
+//         System.out.println("Hello "+ name);
+//     }
+//     public static void main(String[] args) {
+//         greet("Ganesh");
+//     }
+// }
+
+// public class Main {
+//     static int add(int a, int b){
+//        return a+b;
+//     }
+//     static double add(double a, double b){
+//         return a+b;
+//     } 
+//     public static void main(String[] args) {
+//         System.out.println(add(10,20));
+//         System.out.println(add(10.5,20.5));
+//     }
+// }
+
+// public class Main {
+
+//     static void show(){
+//         int x=10;
+//         System.out.println(x);
+//     }
+//     public static void main(String[] args) {
+//         show();
+//     }
+// }
+
+// public class Main {
+//     static void countdown(int n){
+//         if(n>0){
+//             System.out.println(n);
+//             countdown(n-1);
+//         }
+//     }
+//     public static void main(String[] args){
+//         countdown(5);
+//     }
+// }
