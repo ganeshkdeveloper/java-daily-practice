6ee761e4c16a328c5fafcda3131b36b3b327783d
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-16 10:14:25 +0530

    today

--- Day12/m.java
+++ Day12/m.java
@@ -1,12 +1,24 @@
 package Day12;
 
+// public class m {
+//     public static void main(String[] args){
+//     String  name = "Ganesh";
+//        int  age = 26;
+//       double  salary = 25000.50;
+//         System.out.println("Name: "+name);
+//         System.out.println("Age: "+age);
+//         System.out.println("Salary: "+salary);
+//     }
+// }
+
 public class m {
-    public static void main(String[] args){
-    String  name = "Ganesh";
-       int  age = 26;
-      double  salary = 25000.50;
-        System.out.println("Name: "+name);
-        System.out.println("Age: "+age);
-        System.out.println("Salary: "+salary);
+
+    public static void main(String[] args) {
+        char grade = 'A';
+    boolean isJavaFun = true;
+    float marks = 95.5f;
+    System.out.println("Grade: "+grade);
+    System.out.println("Java Fun: "+isJavaFun);
+    System.out.println("Marks: "+marks);
     }
 }
