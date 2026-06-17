77e53d1daa3144b4e0436c3c6f673d6c24206de4
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-17 12:41:50 +0530

    java

---
+++ Day19/Main.java
@@ -0,0 +1,35 @@
+// package Day19;
+
+// import java.util.ArrayList;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         ArrayList<String> names=new ArrayList<>();
+//         names.add("Ganesh");
+//         names.add("Java");
+//         names.add("Python");
+//         System.out.println(names);
+//         System.out.println(names.get(1));
+//         System.out.println(names.remove(1));
+//         System.out.println(names.size());
+//         System.out.println(names.set(1,"SpringBoot"));
+//         System.out.println(names);
+        
+//     }
+// }
+package Day19;
+
+// import java.util.ArrayList;
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//         ArrayList<String> names=new ArrayList<>();
+//         names.add("Ganesh");
+//         names.add("Java");
+//         names.add("Python");
+//         for(String num:names){
+//             System.out.println(num);
+//         }
+//     }
+// }
