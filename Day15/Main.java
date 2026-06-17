e83809386fe2f231383b680f108a4b3c6d4dd103
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-17 11:13:47 +0530

    tody17

--- Day15/Main.java
+++ Day15/Main.java
@@ -1,18 +1,34 @@
 package Day15;
 
-public class Main {
-    static int countGreaterThanTen(int[] arr){
-        int count=0;
-        for(int num:arr){
-            if(num>10){
-                count++;
-            }
-        }
-        return count;
-    }
-public static void main(String[] args) {
-    int[] arr = {5, 12, 8, 15, 20};
-    System.out.println(countGreaterThanTen(arr));
-}
+// public class Main {
+//     static int countGreaterThanTen(int[] arr){
+//         int count=0;
+//         for(int num:arr){
+//             if(num>10){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// public static void main(String[] args) {
+//     int[] arr = {5, 12, 8, 15, 20};
+//     System.out.println(countGreaterThanTen(arr));
+// }
     
-}
+// }
+
+// public class Main {
+//     static int countLessThanTen(int[] arr){
+//         int count=0;
+//         for(int num:arr){
+//             if(num<10){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr = {5, 12, 8, 15, 20, 3};
+//         System.out.println(countLessThanTen(arr));
+//     }
+// }
