b1fbb5ffef30f94ade1d5b8b6c6bcceec4cf01d2
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-17 14:10:58 +0530

    hh

--- Day19/Main.java
+++ Day19/Main.java
@@ -20,6 +20,15 @@
 package Day19;
 
 // import java.util.ArrayList;
+// import java.util.HashMap;
+// import java.util.HashSet;
+// import java.util.Iterator;
+// import java.util.LinkedHashSet;
+// import java.util.LinkedList;
+// import java.util.TreeMap;
+// import java.util.TreeSet;
+
+// import java.util.ArrayList;
 
 // public class Main {
 
@@ -30,6 +39,88 @@ package Day19;
 //         names.add("Python");
 //         for(String num:names){
 //             System.out.println(num);
+//         }
+//     }
+// }
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//         LinkedList<String> names=new LinkedList<>();
+//         names.add("Java");
+//         names.add("Python");
+//         names.addFirst("Ganesh");
+//         names.addLast("SpringBoot");
+//         System.out.println(names);
+//     }
+// }
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//         HashSet<String> languages=new HashSet<>();
+//         languages.add("Java");
+//         languages.add("Python");
+//         languages.add("Java");
+//         System.out.println(languages);
+//     }
+// }
+// public class Main {
+
+//     public static void main(String[] args) {
+//         TreeSet<String> languages=new TreeSet<>();
+//          languages.add("Python");
+//         languages.add("Java");
+//         languages.add("Spring Boot");
+//         languages.add("C");
+//         System.out.println(languages);
+//     }
+// }
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//         LinkedHashSet<String> languages=new LinkedHashSet<>();
+//       languages.add("Python");
+//         languages.add("Java");
+//         languages.add("Spring Boot");
+//         languages.add("C");
+//         System.out.println(languages);
+
+//     }
+// }
+// public class Main {
+
+//     public static void main(String[] args) {
+//         HashMap<Integer,String> students=new HashMap<>();
+//         students.put(101,"Ganesh");
+//         students.put(102,"Java");
+//         students.put(103,"Python");
+//         System.out.println(students.get(101));
+//         System.out.println(students.get(103));  
+
+//     }
+// }
+// public class Main {
+
+//     public static void main(String[] args) {
+//         TreeMap<Integer,String> students=new TreeMap<>();
+//         students.put(103, "Python");
+//         students.put(101, "Ganesh");
+//         students.put(102, "Java");
+//         System.out.println(students);
+//     }
+// }
+// public class Main {
+
+//     public static void main(String[] args) {
+//         ArrayList<String> names=new ArrayList<>();
+//         names.add("Ganesh");
+//         names.add("Java");
+//         names.add("Python");
+//         Iterator<String> it=names.iterator();
+//         while (it.hasNext()) {
+//             System.out.println(it.next());
 //         }
 //     }
 // }
