f2b03bc593a72b8f47761b2f71f66794c30dcc11
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-17 14:36:22 +0530

    17626

---
+++ Day20/Main.java
@@ -0,0 +1,90 @@
+package Day20;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         int a=100;
+//         Integer b=a;
+//         int c=b;
+//         System.out.println(a);
+//         System.out.println(b);
+//         System.out.println(c);
+//     }
+// }
+
+// public class Main {
+//     static <T> void printData(T data){
+//         System.out.println(data);
+//     }
+//     public static void main(String[] args) {
+//         printData("Ganesh");
+//         printData(100);
+//         printData(99.5);
+//     }
+// }
+// class Vehicle{
+//     void start(){
+//         System.out.println("Vehicle Started");
+//     }
+// }
+// class Car extends Vehicle{
+//     @Override
+//     void start(){
+//         System.out.println("Car Started");
+//     }
+// }
+// public class Main {
+
+//     public static void main(String[] args) {
+//         Car c=new Car();
+//         c.start();
+//     }
+// }
+// import java.util.regex.Pattern;
+// public class Main {
+
+//     public static void main(String[] args) {
+//         String text="Java";
+//         boolean result=Pattern.matches("Java",text);
+//         System.out.println(result);
+//     }
+// }
+// class MyThread extends Thread{
+//     public void run(){
+//         System.out.println("Thread Running");
+//     }
+// }
+// public class Main {
+
+//     public static void main(String[] args) {
+//         MyThread t1=new MyThread();
+//         t1.start();
+//     }
+// }
+// interface Addition{
+//     int add(int a,int b);
+// }
+// public class Main {
+
+//     public static void main(String[] args) {
+//         Addition obj=(a,b)->a+b;
+//         System.out.println(obj.add(10,20));
+//     }
+// }
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//         ArrayList<Integer> numbers=new ArrayList<>();
+//         numbers.add(50);
+//         numbers.add(10);
+//         numbers.add(30);
+//         numbers.add(20);
+//         Collections.sort(numbers);
+//         System.out.println(numbers);
+//         Collections.sort(numbers, Collections.reverseOrder());
+//         System.out.println(numbers);
+//     }
+// }
