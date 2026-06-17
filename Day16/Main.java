bf6b028e59750e2ee0ba55d51c0126b0bea95b7d
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-17 11:26:20 +0530

    lasttoday

---
+++ Day16/Main.java
@@ -0,0 +1,34 @@
+package Day16;
+
+// import java.time.LocalDate;
+// import java.time.LocalDateTime;
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter a number: ");
+//         int a=sc.nextInt();
+//         System.out.println("you entered: "+ a);
+//         sc.close();
+//     }
+// }
+// public class Main {
+
+//     public static void main(String[] args) {
+//         LocalDateTime today=LocalDateTime.now();
+//         System.out.println(today);
+//     }
+// }
+
+// public class Main {
+
+// public static void main(String[] args) {
+//     try {
+//         int[] arr={1,2,3};
+//         System.out.println(arr[5]);
+//     } catch (Exception e) {
+//         System.out.println("Error occurred");
+//     }
+// }    
+// }
