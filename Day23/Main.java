8408a940e47204420a8a8f09e435f2d6645fadb9
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-19 09:33:47 +0530

    19626

--- Day23/Main.java
+++ Day23/Main.java
@@ -1,17 +1,33 @@
 package Day23;
 
-public class Main {
-    static int countMultiplesOfTwo(int[] arr){
-        int count=0;
-        for(int num:arr){
-            if(num%2==0){
-                count++;
-            }
-        }
-        return count;
-    }
-    public static void main(String[] args) {
-        int[] arr = {2, 5, 8, 11, 14};
-        System.out.println(countMultiplesOfTwo(arr));
-    }
-}
+// public class Main {
+//     static int countMultiplesOfTwo(int[] arr){
+//         int count=0;
+//         for(int num:arr){
+//             if(num%2==0){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr = {2, 5, 8, 11, 14};
+//         System.out.println(countMultiplesOfTwo(arr));
+//     }
+// }
+
+// public class Main {
+//     static int countNumbersGreaterThanFive(int[] arr){
+//         int count=0;
+//         for(int num:arr){
+//             if(num>5){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr = {2, 5, 8, 11, 14, 3};
+//         System.out.println(countNumbersGreaterThanFive(arr));
+//     }
+// }
