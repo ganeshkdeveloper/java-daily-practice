b4762e216167d0ca61a782af20942f260101f726
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-20 22:07:55 +0530

    20626

---
+++ Day24/Main.java
@@ -0,0 +1,12 @@
+package Day24;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner s=new Scanner(System.in);
+      String a=s.nextLine();
+      System.out.printf("Hello, %s! Welcome to Programming.",a);
+      s.close();
+    }
+}
