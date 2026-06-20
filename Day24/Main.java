3848b714f9b2c3925fc4a6d1517b20ee741c3527
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-20 23:12:24 +0530

    20

--- Day24/Main.java
+++ Day24/Main.java
@@ -24,12 +24,12 @@ import java.util.Scanner;
 //     }
 // }
 
-public class Main {
-    public static void main(String[] args) {
-      Scanner sc=new Scanner(System.in);
-      int a=sc.nextInt();
-      int b=sc.nextInt();
-      System.out.printf("The sum is %d",a+b);
-      sc.close();
-    }
-}
+// public class Main {
+//     public static void main(String[] args) {
+//       Scanner sc=new Scanner(System.in);
+//       int a=sc.nextInt();
+//       int b=sc.nextInt();
+//       System.out.printf("The sum is %d",a+b);
+//       sc.close();
+//     }
+// }
