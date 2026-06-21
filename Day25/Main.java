3244a22681af5664a5cd6320b0f0aaac9c0b8943
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-21 20:34:50 +0530

    21626

--- Day25/Main.java
+++ Day25/Main.java
@@ -12,3 +12,14 @@ package Day25;
 //         System.out.println("Sum = " + (a + b));
 //     }
 // }
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double r = sc.nextDouble();
+
+        System.out.printf("Area = %.2f", 3.14 * r * r);
+    }
+}
