6bd20d6688163881e5abd510713cc6a91b907684
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-21 20:34:14 +0530

    21626

--- Day25/Main.java
+++ Day25/Main.java
@@ -1,14 +1,14 @@
 package Day25;
 
-import java.util.*;
+// import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
 
-        System.out.println("Sum = " + (a + b));
-    }
-}
+//         System.out.println("Sum = " + (a + b));
+//     }
+// }
