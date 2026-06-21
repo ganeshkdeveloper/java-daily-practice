8a39cc0af007236ecadbca29afae12c70239e135
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-21 20:35:32 +0530

    21626

--- Day25/Main.java
+++ Day25/Main.java
@@ -12,14 +12,14 @@ package Day25;
 //         System.out.println("Sum = " + (a + b));
 //     }
 // }
-import java.util.*;
+// import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
 
-        double r = sc.nextDouble();
+//         double r = sc.nextDouble();
 
-        System.out.printf("Area = %.2f", 3.14 * r * r);
-    }
-}
+//         System.out.printf("Area = %.2f", 3.14 * r * r);
+//     }
+// }
