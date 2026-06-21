b64a6c4d9c6449fec7f2c9c70b316d24875082b1
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-21 20:36:11 +0530

    21666626

--- Day25/Main.java
+++ Day25/Main.java
@@ -23,3 +23,16 @@ package Day25;
 //         System.out.printf("Area = %.2f", 3.14 * r * r);
 //     }
 // }
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        System.out.printf("Average = %.1f", (a + b + c) / 3);
+    }
+}
