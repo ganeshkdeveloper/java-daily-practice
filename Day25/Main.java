b61aa818db95dbf6728a99edfd346b10866e0292
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-21 20:37:52 +0530

    21626

--- Day25/Main.java
+++ Day25/Main.java
@@ -48,3 +48,18 @@ package Day25;
 //         System.out.println("Remainder = " + (a % b));
 //     }
 // }
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double p = sc.nextDouble();
+        double r = sc.nextDouble();
+        double t = sc.nextDouble();
+
+        double si = (p * r * t) / 100;
+
+        System.out.printf("Simple Interest = %.1f", si);
+    }
+}
