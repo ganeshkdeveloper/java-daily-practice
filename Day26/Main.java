c799c06092f1a7e04c7d36046cb542cd8219bab1
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-22 09:36:47 +0530

    22626

---
+++ Day26/Main.java
@@ -0,0 +1,16 @@
+package Day26;
+public class Main {
+    static int countNumbersLessThanZero(int[] arr){
+        int count=0;
+        for(int num:arr){
+            if(num<0){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, -2, 8, -7, 10, -1};
+        System.out.println(countNumbersLessThanZero(arr));
+    }
+}
