82cfb2b27a21d144929d636c4cefd22de566d215
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-24 09:48:02 +0530

    24626

--- Day28/Main.java
+++ Day28/Main.java
@@ -16,18 +16,18 @@ package Day28;
     //     }
     // }
 
-    public class Main {
-    static int countNumbersEqualToThree(int[] arr){
-        int count=0;
-        for(int num:arr){
-            if(num==3){
-                count++;
-            }
-        }
-        return count;
-    }
-        public static void main(String[] args) {
-            int[] arr = {3, 5, 3, 8, 10, 3};
-            System.out.println(countNumbersEqualToThree(arr));
-        }
-    }
+    // public class Main {
+    // static int countNumbersEqualToThree(int[] arr){
+    //     int count=0;
+    //     for(int num:arr){
+    //         if(num==3){
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+    //     public static void main(String[] args) {
+    //         int[] arr = {3, 5, 3, 8, 10, 3};
+    //         System.out.println(countNumbersEqualToThree(arr));
+    //     }
+    // }
