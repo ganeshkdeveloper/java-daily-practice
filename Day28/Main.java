06de841aae1739d6127f3c277f1e5c4908512b9f
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-24 12:08:38 +0530

    24626

--- Day28/Main.java
+++ Day28/Main.java
@@ -41,16 +41,16 @@ package Day28;
     //           System.out.println(reverse("hello"));
     //     }
     // }
-    public class Main {
+    // public class Main {
     
-        static boolean isPrime(int n){
-            if(n<=1) return false;
-            for(int i=2;i*i<=n;i++){
-                if(n%i==0) return false;
-            }
-            return true;
-        }
-        public static void main(String[] args) {
-            System.out.println(isPrime(17));
-        }
-    }
+    //     static boolean isPrime(int n){
+    //         if(n<=1) return false;
+    //         for(int i=2;i*i<=n;i++){
+    //             if(n%i==0) return false;
+    //         }
+    //         return true;
+    //     }
+    //     public static void main(String[] args) {
+    //         System.out.println(isPrime(17));
+    //     }
+    // }
