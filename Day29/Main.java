6af615117876e329de2d84ce2944c8d82b6c1297
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-25 11:28:51 +0530

    25626

---
+++ Day29/Main.java
@@ -0,0 +1,17 @@
+
+package Day29;
+public class Main {
+    static int countNumbersEqualToEight(int[] arr){
+        int count=0;
+        for(int num:arr){
+            if(num==8){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr = {8, 5, 8, 10, 3, 8};
+        System.out.println(countNumbersEqualToEight(arr));
+    }
+}
