a88fd906ad1efe6349e78bc99f61435c3839f710
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-25 14:42:16 +0530

    25626

--- Day29/Main.java
+++ Day29/Main.java
@@ -48,16 +48,16 @@ import java.util.Scanner;
 //         System.out.println(sumNumbersGreaterThanTen(arr));
 //     }
 // }
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int num=sc.nextInt();
-        if(num%2==0){
-            System.out.println("Even");
-        }else{
-            System.out.println("Odd");
-        }
-        sc.close();
-    }
-}
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int num=sc.nextInt();
+//         if(num%2==0){
+//             System.out.println("Even");
+//         }else{
+//             System.out.println("Odd");
+//         }
+//         sc.close();
+//     }
+// }
