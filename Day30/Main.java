4e9198e34692fadfd681299b8cd37f1831062532
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-26 09:46:48 +0530

    26626

--- Day30/Main.java
+++ Day30/Main.java
@@ -14,3 +14,18 @@ package Day30;
 //         System.out.println(sumNumbersDivisibleByThree(arr));
 //     }
 // }
+public class Main {
+static int sumEvenNumbersGreaterThanTen(int[] arr){
+    int sum=0;
+    for(int num:arr){
+        if(num%2==0&&num>10){
+            sum+=num;
+        }
+    }
+    return sum;
+}
+    public static void main(String[] args) {
+        int[] arr = {2, 12, 7, 14, 9, 20};
+        System.out.println(sumEvenNumbersGreaterThanTen(arr));
+    }
+}
