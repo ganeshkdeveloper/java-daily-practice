ae4d834a9e0dc917f44fc0d94c183665638e4248
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-27 16:56:31 +0530

    27626

--- Day31/Main.java
+++ Day31/Main.java
@@ -30,3 +30,25 @@ package Day31;
 //         System.out.println(findIndex(arr, 12));
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+    static int findSecondLargest(int[] arr){
+        int largest=arr[0];
+        int SecondLargest=arr[0];
+        for(int num:arr){
+            if(num>largest){
+                SecondLargest=largest;
+                largest=num;
+            }else if(num>SecondLargest&&num!=largest){
+                SecondLargest=num;
+            }
+        }
+        return SecondLargest;
+    }
+    public static void main(String[] args) {
+        int[] arr = {8, 3, 12, 1, 6, 10};
+        System.out.println(findSecondLargest(arr));
+    }
+}
