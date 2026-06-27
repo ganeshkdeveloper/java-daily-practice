128f894953d9ab97ef21f3efdf3f7dc4ccd9e155
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-27 16:40:31 +0530

    27626

--- Day31/Main.java
+++ Day31/Main.java
@@ -16,17 +16,17 @@ package Day31;
 /**
  * Main
  */
-public class Main {
-    static int findIndex(int[] arr, int target){
-        for(int i = 0; i < arr.length; i++){
-            if (arr[i]==target){
-                return i;
-            }
-        }
-        return-1;
-    }
-    public static void main(String[] args) {
-        int[] arr = {5, 8, 12, 3, 9};
-        System.out.println(findIndex(arr, 12));
-    }
-}
+// public class Main {
+//     static int findIndex(int[] arr, int target){
+//         for(int i = 0; i < arr.length; i++){
+//             if (arr[i]==target){
+//                 return i;
+//             }
+//         }
+//         return-1;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr = {5, 8, 12, 3, 9};
+//         System.out.println(findIndex(arr, 12));
+//     }
+// }
