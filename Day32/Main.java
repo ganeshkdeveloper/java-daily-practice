e267309586cbd94ac8110ddce2bd461f57b130ca
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-28 22:17:18 +0530

    27626

--- Day32/Main.java
+++ Day32/Main.java
@@ -6,3 +6,21 @@ package Day32;
 //         System.out.println(number);
 //     }
 // }
+
+/**
+ * Main
+ */
+public class Main {
+    static int findFirstPositive(int[] arr){
+        for(int num:arr){
+            if(num>0){
+                return num;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+         int[] arr = {-5, -2, 0, 7, 10};
+            System.out.println(findFirstPositive(arr));
+    }
+}
