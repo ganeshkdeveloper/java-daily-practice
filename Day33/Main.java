84a7a860e56184ab900dde5ce7c01739c2c11e5c
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-29 09:30:04 +0530

    29626

---
+++ Day33/Main.java
@@ -0,0 +1,16 @@
+package Day33;
+
+public class Main {
+    static int findFirstIndex(int[] arr, int target){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 12, 8, 3, 8};
+        System.out.println(findFirstIndex(arr, 8));
+    }
+}
