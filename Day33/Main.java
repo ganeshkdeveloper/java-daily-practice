c8b2ad459a7ee2b0726101e1f1d908cb127030f2
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-29 11:06:40 +0530

    29626

--- Day33/Main.java
+++ Day33/Main.java
@@ -45,3 +45,21 @@ package Day33;
 //         System.out.println(findFirstEven(arr));
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+    static int findLargestEvenNumber(int[] arr){
+        int LargestEvenNumber=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==0&&arr[i]>arr[0]){
+                LargestEvenNumber=arr[i];
+            }
+        }
+        return LargestEvenNumber;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 18, 22, 25, 30, 17};
+        System.out.println(findLargestEvenNumber(arr));
+    }
+}
