38782702627f1b4a3fb5e1947b3307518ba1a7aa
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-30 09:58:55 +0530

    29626

---
+++ Day34/Main.java
@@ -0,0 +1,19 @@
+package Day34;
+public class Main {
+    static int findSmallestOddNumber(int[] arr){
+        int smallestOdd=Integer.MAX_VALUE;
+        for(int num:arr){
+            if(num%2!=0&&num<smallestOdd){
+                smallestOdd=num;
+            }
+        }
+        if (smallestOdd == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return smallestOdd;
+    }
+    public static void main(String[] args) {
+         int[] arr = {8, 11, 5, 14, 3, 10};
+        System.out.println(findSmallestOddNumber(arr));
+    }
+}
