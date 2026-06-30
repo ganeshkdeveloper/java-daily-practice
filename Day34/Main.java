17741b1e045f7135f5bec381f1f8515bdfa3ca29
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-06-30 10:48:06 +0530

    30626

--- Day34/Main.java
+++ Day34/Main.java
@@ -61,11 +61,11 @@ package Day34;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        int a=10;
-        int b=20;
-        System.out.println(a+b);
-    }
-}
+//     public static void main(String[] args) {
+//         int a=10;
+//         int b=20;
+//         System.out.println(a+b);
+//     }
+// }
