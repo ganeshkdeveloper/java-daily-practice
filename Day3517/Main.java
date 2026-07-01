49bbfb8ccd0395e5465d9040904a15208f9e1dcc
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-01 10:08:02 +0530

    30626

--- Day3517/Main.java
+++ Day3517/Main.java
@@ -58,11 +58,11 @@ package Day3517;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        int a=20;
-        int b=30;
-        System.out.println(a+b);
-    }
-}
+//     public static void main(String[] args) {
+//         int a=20;
+//         int b=30;
+//         System.out.println(a+b);
+//     }
+// }
