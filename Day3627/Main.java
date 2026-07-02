ae81de493c2c7647f32b2f1cc7eb74256f4361ba
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-02 09:45:18 +0530

    30626

--- Day3627/Main.java
+++ Day3627/Main.java
@@ -15,3 +15,21 @@ package Day3627;
 //         System.out.println(findLastEvenLessThanTwenty(arr));
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+    static int findFirstPositiveEvenGreaterThanTen(int[] arr){
+     
+        for(int num:arr){
+            if(num>0&&num%2==0&&num>10){
+               return num;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {-5, 7, 8, 12, 15, 20};
+        System.out.println(findFirstPositiveEvenGreaterThanTen(arr));
+    }
+}
