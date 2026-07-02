4e4873c10f7c43314d1647e70c626645b9e0579a
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-02 09:38:50 +0530

    30626

--- Day3627/Main.java
+++ Day3627/Main.java
@@ -1,17 +1,17 @@
 package Day3627;
 
-public class Main {
-    static int findLastEvenLessThanTwenty(int[] arr){
-        int last=-1;
-        for(int num:arr){
-           if(num<20&&num%2==0){
-            last=num;
-           } 
-        }
-        return last;
-    }
-    public static void main(String[] args) {
-        int[] arr = {22, 18, 25, 14, 9, 16, 30};
-        System.out.println(findLastEvenLessThanTwenty(arr));
-    }
-}
+// public class Main {
+//     static int findLastEvenLessThanTwenty(int[] arr){
+//         int last=-1;
+//         for(int num:arr){
+//            if(num<20&&num%2==0){
+//             last=num;
+//            } 
+//         }
+//         return last;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr = {22, 18, 25, 14, 9, 16, 30};
+//         System.out.println(findLastEvenLessThanTwenty(arr));
+//     }
+// }
