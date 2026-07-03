ad29024841de732ff7170aa96158a5a9eb33efe8
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-03 09:53:38 +0530

    030726

--- Day3737/Main.java
+++ Day3737/Main.java
@@ -17,22 +17,22 @@ package Day3737;
 /**
  * Main
  */
-public class Main {
-    static int findSmallestPositiveEvenNumber(int[] arr){
-        int smallest=Integer.MAX_VALUE;
-        for(int num:arr){
-            if(num>0&&num%2==0&&num<smallest){
-                smallest=num;
-            }
-        }
-        if(smallest==Integer.MAX_VALUE){
-            return -1;
-        }
-        return smallest;
-    }
+// public class Main {
+//     static int findSmallestPositiveEvenNumber(int[] arr){
+//         int smallest=Integer.MAX_VALUE;
+//         for(int num:arr){
+//             if(num>0&&num%2==0&&num<smallest){
+//                 smallest=num;
+//             }
+//         }
+//         if(smallest==Integer.MAX_VALUE){
+//             return -1;
+//         }
+//         return smallest;
+//     }
 
-    public static void main(String[] args) {
-        int[] arr = {-4, 18, 7, 12, 3, 20, 6};
-        System.out.println(findSmallestPositiveEvenNumber(arr));
-    }
-}
+//     public static void main(String[] args) {
+//         int[] arr = {-4, 18, 7, 12, 3, 20, 6};
+//         System.out.println(findSmallestPositiveEvenNumber(arr));
+//     }
+// }
