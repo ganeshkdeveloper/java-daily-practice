d9ef7b5caaa3f4a9102941bf7f7bea531268f89a
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-04 22:41:08 +0530

    4726

--- Day3847/Main.java
+++ Day3847/Main.java
@@ -19,11 +19,11 @@ package Day3847;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        int num1=10;
-        int num2=20;
-        System.out.println(num1+num2);
-    }
-}
+//     public static void main(String[] args) {
+//         int num1=10;
+//         int num2=20;
+//         System.out.println(num1+num2);
+//     }
+// }
