bf5a131b76966005547b645e52ccaefd93fd494e
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-05 15:15:48 +0530

    5726

--- Day3957/Main.java
+++ Day3957/Main.java
@@ -1,7 +1,7 @@
 package Day3957;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Pls do with consistantly");
-    }
-}
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Pls do with consistantly");
+//     }
+// }
