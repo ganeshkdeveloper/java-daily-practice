f3ef5a28176359b25e657382504ff1734e659ad5
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-05 15:21:09 +0530

    5726

--- Day3957/Main.java
+++ Day3957/Main.java
@@ -8,9 +8,9 @@ package Day3957;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("constantly leads to freedom");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("constantly leads to freedom");
+//     }
+// }
