b1f157d787200bf86a6199107d0e0bf3c1f3cbf1
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-05 15:22:56 +0530

    5726

--- Day3957/Main.java
+++ Day3957/Main.java
@@ -14,3 +14,56 @@ package Day3957;
 //         System.out.println("constantly leads to freedom");
 //     }
 // }
+/**
+ * Main
+ */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Choose Operation:");
+        System.out.println("1. Addition (+)");
+        System.out.println("2. Subtraction (-)");
+        System.out.println("3. Multiplication (*)");
+        System.out.println("4. Division (/)");
+
+        System.out.print("Enter your choice (1-4): ");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Result = " + (num1 + num2));
+                break;
+
+            case 2:
+                System.out.println("Result = " + (num1 - num2));
+                break;
+
+            case 3:
+                System.out.println("Result = " + (num1 * num2));
+                break;
+
+            case 4:
+                if (num2 != 0) {
+                    System.out.println("Result = " + (num1 / num2));
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid Choice!");
+        }
+
+        sc.close();
+    }
+}
