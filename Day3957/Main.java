afd77ed400acedf85d7fd676d5a61a36ed85db8a
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-05 15:26:55 +0530

    4726

--- Day3957/Main.java
+++ Day3957/Main.java
@@ -70,9 +70,9 @@ package Day3957;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Next project done");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Next project done");
+//     }
+// }
