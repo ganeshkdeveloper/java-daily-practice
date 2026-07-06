a7b7a9d2bd5fbbf9a0421f5eaf501a47a0ea9ad6
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-06 09:40:42 +0530

    6726

--- Day4067/Main.java
+++ Day4067/Main.java
@@ -18,18 +18,18 @@ package Day4067;
 /**
  * Main
  */
-public class Main {
-    static int countNegativeOddNumbersLessThanMinusFive(int[] arr){
-        int count=0;
-        for(int num:arr){
-            if(num%2!=0&&num<0&&num<-5){
-                count++;
-            }
-        }
-        return count;
-    }
-    public static void main(String[] args) {
-        int[] arr={-3, -7, -9, -4, -11, 6};
-        System.out.println(countNegativeOddNumbersLessThanMinusFive(arr));
-    }
-}
+    // public class Main {
+    //     static int countNegativeOddNumbersLessThanMinusFive(int[] arr){
+    //         int count=0;
+    //         for(int num:arr){
+    //             if(num%2!=0&&num<0&&num<-5){
+    //                 count++;
+    //             }
+    //         }
+    //         return count;
+    //     }
+    //     public static void main(String[] args) {
+    //         int[] arr={-3, -7, -9, -4, -11, 6};
+    //         System.out.println(countNegativeOddNumbersLessThanMinusFive(arr));
+    //     }
+    // }
