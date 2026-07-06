9f927754713ed715c8fc0bf450ff96699100d300
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-06 09:31:43 +0530

    6726

--- Day4067/Main.java
+++ Day4067/Main.java
@@ -1,17 +1,17 @@
 package Day4067;
 
-public class Main {
-    static int sumNegativeEvenNumbersGreaterThanMinusTwenty(int[] arr){
-        int sum=0;
-        for(int num:arr){
-            if(num%2==0&&num<0&&num>-20){
-                sum+=num;
-            }
-        }
-        return sum;
-    }
-    public static void main(String[] args) {
-        int[] arr={-25, -18, -10, 5, -6, 12};
-        System.out.println(sumNegativeEvenNumbersGreaterThanMinusTwenty(arr));
-    }
-}
+// public class Main {
+//     static int sumNegativeEvenNumbersGreaterThanMinusTwenty(int[] arr){
+//         int sum=0;
+//         for(int num:arr){
+//             if(num%2==0&&num<0&&num>-20){
+//                 sum+=num;
+//             }
+//         }
+//         return sum;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr={-25, -18, -10, 5, -6, 12};
+//         System.out.println(sumNegativeEvenNumbersGreaterThanMinusTwenty(arr));
+//     }
+// }
