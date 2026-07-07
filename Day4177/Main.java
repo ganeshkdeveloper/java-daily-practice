690057d225902d4d693f672954a963feade18a09
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-07 09:45:18 +0530

    7726

--- Day4177/Main.java
+++ Day4177/Main.java
@@ -5,18 +5,18 @@ package Day4177;
 /**
  * Main
  */
-public class Main {
-    static int findLargestEvenNumberLessThanFifty(int[] arr){
-        int largest=-1;
-        for(int num:arr){
-            if(num%2==0&&num<50&&num>largest){
-                largest=num;
-            }
-        }
-        return largest;
-    }
-    public static void main(String[] args) {
-        int[] arr = {52, 18, 44, 49, 36, 60};
-        System.out.println(findLargestEvenNumberLessThanFifty(arr));
-    }
-}
+// public class Main {
+//     static int findLargestEvenNumberLessThanFifty(int[] arr){
+//         int largest=-1;
+//         for(int num:arr){
+//             if(num%2==0&&num<50&&num>largest){
+//                 largest=num;
+//             }
+//         }
+//         return largest;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr = {52, 18, 44, 49, 36, 60};
+//         System.out.println(findLargestEvenNumberLessThanFifty(arr));
+//     }
+// }
