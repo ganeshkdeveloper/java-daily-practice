8c65b0a36678a829a6929b993d64615fb34c6db1
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-07 09:52:56 +0530

    7726

--- Day4177/Main.java
+++ Day4177/Main.java
@@ -20,3 +20,23 @@ package Day4177;
 //         System.out.println(findLargestEvenNumberLessThanFifty(arr));
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+    static int findSmallestNegativeOddGreaterThanMinusTwenty(int[] arr){
+        int smallest=-1;
+        for(int num:arr){
+            if(num%2!=0&&num<0&&num>-20){
+                if(smallest==-1||num<smallest){
+                    smallest=num;
+                }
+            }
+        }
+        return smallest;
+    }
+    public static void main(String[] args) {
+        int[] arr={-25, -19, -7, -15, 8, -3};
+        System.out.println(findSmallestNegativeOddGreaterThanMinusTwenty(arr));
+    }
+}
