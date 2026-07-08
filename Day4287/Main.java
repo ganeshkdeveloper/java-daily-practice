1630fcb5bdc4caf88bd9010dc0a3ad61c4a7ae7f
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-08 11:58:54 +0530

    080726

--- Day4287/Main.java
+++ Day4287/Main.java
@@ -1,20 +1,20 @@
 package Day4287;
 
-public class Main {
-    static int findLargestNegativeEvenNumber(int[] arr){
-        int largest=Integer.MIN_VALUE;
-        for(int num:arr){
-            if(num<0&&num%2==0&&num>largest){
-                largest=num;
-            }
-        }
-        if (largest == Integer.MIN_VALUE) {
-    return -1;
-}
-        return largest;
-    }
-    public static void main(String[] args) {
-        int[] arr = {-12, -8, -20, -5, 4, -2};
-        System.out.println(findLargestNegativeEvenNumber(arr));
-    }
-}
+// public class Main {
+//     static int findLargestNegativeEvenNumber(int[] arr){
+//         int largest=Integer.MIN_VALUE;
+//         for(int num:arr){
+//             if(num<0&&num%2==0&&num>largest){
+//                 largest=num;
+//             }
+//         }
+//         if (largest == Integer.MIN_VALUE) {
+//     return -1;
+// }
+//         return largest;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr = {-12, -8, -20, -5, 4, -2};
+//         System.out.println(findLargestNegativeEvenNumber(arr));
+//     }
+// }
