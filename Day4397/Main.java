14c7a2ac845003f40508a02b31f078ef1a59ac25
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-09 10:26:37 +0530

    090726

--- Day4397/Main.java
+++ Day4397/Main.java
@@ -1,26 +1,26 @@
 package Day4397;
-public class Main {
-    static int findSecondLargestNegativeEvenNumber(int[] arr){
-        int firstLargest=Integer.MIN_VALUE;
-        int secondLargest=Integer.MIN_VALUE;
-        for(int num:arr){
-            if(num%2==0&&num<0){
-                if(num>firstLargest){
-                    secondLargest=firstLargest;
-                    firstLargest=num;
-                }else if(num>secondLargest&&num!=firstLargest){
-                    secondLargest=num;
-                }
-            }
+// public class Main {
+//     static int findSecondLargestNegativeEvenNumber(int[] arr){
+//         int firstLargest=Integer.MIN_VALUE;
+//         int secondLargest=Integer.MIN_VALUE;
+//         for(int num:arr){
+//             if(num%2==0&&num<0){
+//                 if(num>firstLargest){
+//                     secondLargest=firstLargest;
+//                     firstLargest=num;
+//                 }else if(num>secondLargest&&num!=firstLargest){
+//                     secondLargest=num;
+//                 }
+//             }
            
-            }
-             if(secondLargest==Integer.MIN_VALUE){
-                return -1;
-        }
-        return secondLargest;
-    }
-    public static void main(String[] args) {
-        int[] arr = {-12, -8, -20, -2, 5, 10};
-        System.out.println(findSecondLargestNegativeEvenNumber(arr));
-    }
-}
+//             }
+//              if(secondLargest==Integer.MIN_VALUE){
+//                 return -1;
+//         }
+//         return secondLargest;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr = {-12, -8, -20, -2, 5, 10};
+//         System.out.println(findSecondLargestNegativeEvenNumber(arr));
+//     }
+// }
