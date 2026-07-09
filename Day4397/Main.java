d15c0457fd03de578231953b79e338b72bfb67b8
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-09 16:55:44 +0530

    090726

--- Day4397/Main.java
+++ Day4397/Main.java
@@ -100,16 +100,16 @@ import java.util.Scanner;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Your Name: ");
-        String name=sc.nextLine();
-        System.out.println("Enter Your Age: ");
-        int num=sc.nextInt();
-        System.out.println("Your Name is: "+name);
-        System.out.println("Your Age is: "+num);
-        sc.close();
-     }
-}
+//      public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter Your Name: ");
+//         String name=sc.nextLine();
+//         System.out.println("Enter Your Age: ");
+//         int num=sc.nextInt();
+//         System.out.println("Your Name is: "+name);
+//         System.out.println("Your Age is: "+num);
+//         sc.close();
+//      }
+// }
