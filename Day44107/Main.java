afadeaa4a2f08dea54d895239e005dad59ee1766
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-10 10:15:46 +0530

    090726

--- Day44107/Main.java
+++ Day44107/Main.java
@@ -1,15 +1,15 @@
 package Day44107;
-public class Main {
-    static int findSmallestPositiveOddGreaterThanTen(int[] arr){
-        for(int num:arr){
-            if(num>0&&num%2!=0&&num>10){
-                return num;
-            }
-        }
-        return -1;
-    }
-    public static void main(String[] args) {
-        int[] arr = {5, 13, 21, 9, 17, 8};
-        System.out.println(findSmallestPositiveOddGreaterThanTen(arr));
-    }
-}
+// public class Main {
+//     static int findSmallestPositiveOddGreaterThanTen(int[] arr){
+//         for(int num:arr){
+//             if(num>0&&num%2!=0&&num>10){
+//                 return num;
+//             }
+//         }
+//         return -1;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr = {5, 13, 21, 9, 17, 8};
+//         System.out.println(findSmallestPositiveOddGreaterThanTen(arr));
+//     }
+// }
