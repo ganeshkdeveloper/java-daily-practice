30b14154eb0170754803bff3437df8244070c1e3
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-13 10:21:22 +0530

    12726

--- Day46137/Main.java
+++ Day46137/Main.java
@@ -24,3 +24,12 @@ package Day46137;
 //         System.out.println("Solvec");
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World to real Applications");
+    }
+}
