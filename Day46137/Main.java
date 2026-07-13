c68683776ff1898821f5e79697102a087a9a2758
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-13 10:19:09 +0530

    12726

--- Day46137/Main.java
+++ Day46137/Main.java
@@ -1,17 +1,17 @@
 package Day46137;
-public class Main {
-    static int findLargestPositiveOddLessThanHundred(int[] arr){
-        int largest=-1;
-        for(int num:arr){
-            if(num>0&&num%2!=0&&num<100&&num>largest){
-                largest=num;
-            }
-        }
+// public class Main {
+//     static int findLargestPositiveOddLessThanHundred(int[] arr){
+//         int largest=-1;
+//         for(int num:arr){
+//             if(num>0&&num%2!=0&&num<100&&num>largest){
+//                 largest=num;
+//             }
+//         }
     
-        return largest;
-    }
-    public static void main(String[] args) {
-        int[] arr={101, 75, 48, 99, 35, 84};
-        System.out.println(findLargestPositiveOddLessThanHundred(arr));
-    }
-}
+//         return largest;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr={101, 75, 48, 99, 35, 84};
+//         System.out.println(findLargestPositiveOddLessThanHundred(arr));
+//     }
+// }
