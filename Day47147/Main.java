e27624f60c59737495cb854e93e9bae8526cc351
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-14 09:30:00 +0530

    14726

--- Day47147/Main.java
+++ Day47147/Main.java
@@ -1,16 +1,16 @@
 package Day47147;
-public class Main {
-    static int countPositiveEvenNumbersGreaterThanTwenty(int[] arr){
-        int count=0;
-        for(int num:arr){
-            if(num%2==0&&num>0&&num>20){
-                count++;
-            }
-        }
-        return count;
-    }
-    public static void main(String[] args) {
-        int[] arr={18, 22, 35, 40, 21, 26};
-        System.out.println(countPositiveEvenNumbersGreaterThanTwenty(arr));
-    }
-}
+// public class Main {
+//     static int countPositiveEvenNumbersGreaterThanTwenty(int[] arr){
+//         int count=0;
+//         for(int num:arr){
+//             if(num%2==0&&num>0&&num>20){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr={18, 22, 35, 40, 21, 26};
+//         System.out.println(countPositiveEvenNumbersGreaterThanTwenty(arr));
+//     }
+// }
