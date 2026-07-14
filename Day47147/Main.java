a4fcdd006ef47c6e3715c7a06c8fc2d0ad2e6157
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-14 09:41:20 +0530

    14726

--- Day47147/Main.java
+++ Day47147/Main.java
@@ -14,3 +14,23 @@ package Day47147;
 //         System.out.println(countPositiveEvenNumbersGreaterThanTwenty(arr));
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+    static int findIndexOfSmallestNegativeOddNumber(int[] arr){
+        int smallest=Integer.MAX_VALUE;
+        int index=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2!=0&&arr[i]<smallest&&arr[i]<0){
+                smallest=arr[i];
+                index=i;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args) {
+        int[] arr={-7, -15, -3, -21, 10, -5};
+        System.out.println(findIndexOfSmallestNegativeOddNumber(arr));
+    }
+}
