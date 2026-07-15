ff76149e016209c8cd8fa3862a7e43e4d25ccc23
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-15 10:12:20 +0530

    14726

--- Day48157/Main.java
+++ Day48157/Main.java
@@ -1,16 +1,16 @@
 package Day48157;
-public class Main {
-    static int findIndexOfLastNegativeEvenLessThanMinusTen(int[] arr){
-        int index=-1;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]%2==0&&arr[i]<0&&arr[i]<-10){
-                index=i;
-            }
-        }
-        return index;
-    }
-    public static void main(String[] args) {
-        int[] arr={-8, -12, -15, -18, -5, -20};
-        System.out.println(findIndexOfLastNegativeEvenLessThanMinusTen(arr));
-    }
-}
+// public class Main {
+//     static int findIndexOfLastNegativeEvenLessThanMinusTen(int[] arr){
+//         int index=-1;
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]%2==0&&arr[i]<0&&arr[i]<-10){
+//                 index=i;
+//             }
+//         }
+//         return index;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr={-8, -12, -15, -18, -5, -20};
+//         System.out.println(findIndexOfLastNegativeEvenLessThanMinusTen(arr));
+//     }
+// }
