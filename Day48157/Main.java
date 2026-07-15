61521bc8488c98528e537e598760c203c14b081e
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-15 10:25:13 +0530

    14726

--- Day48157/Main.java
+++ Day48157/Main.java
@@ -14,3 +14,23 @@ package Day48157;
 //         System.out.println(findIndexOfLastNegativeEvenLessThanMinusTen(arr));
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+    static int findIndexOfSmallestPositiveEvenGreaterThanTen(int[] arr){
+        int smallest=Integer.MAX_VALUE;
+        int index=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>0&&arr[i]%2==0&&arr[i]>10&&arr[i]<smallest){
+                smallest=arr[i];
+                index=i;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args) {
+        int[] arr={8, 18, 14, 22, 12, 5};
+        System.out.println(findIndexOfSmallestPositiveEvenGreaterThanTen(arr));
+    }
+}
