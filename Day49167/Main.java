efa117a877eddb5223078c5faf04466eb250189d
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-16 10:01:38 +0530

    self made dev

--- Day49167/Main.java
+++ Day49167/Main.java
@@ -1,17 +1,17 @@
 package Day49167;
 
-public class Main {
-    static int sumPositiveOddNumbersWhoseIndexIsEven(int[] arr){
-        int sum=0;
-        for(int i=0;i<arr.length;i++){
-            if(i%2==0&&arr[i]%2!=0){
-                sum+=arr[i];
-            }
-        }
-        return sum;
-    }
-    public static void main(String[] args) {
-        int[] arr={11, 8, 15, 20, 21, 6, 9};
-        System.out.println(sumPositiveOddNumbersWhoseIndexIsEven(arr));
-    }
-}
+// public class Main {
+//     static int sumPositiveOddNumbersWhoseIndexIsEven(int[] arr){
+//         int sum=0;
+//         for(int i=0;i<arr.length;i++){
+//             if(i%2==0&&arr[i]%2!=0){
+//                 sum+=arr[i];
+//             }
+//         }
+//         return sum;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr={11, 8, 15, 20, 21, 6, 9};
+//         System.out.println(sumPositiveOddNumbersWhoseIndexIsEven(arr));
+//     }
+// }
