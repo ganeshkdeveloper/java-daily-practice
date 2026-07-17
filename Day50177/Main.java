2ce608609b9aab654cd0986a76255bc8c3fc1c0c
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-17 09:41:00 +0530

    self made dev

---
+++ Day50177/Main.java
@@ -0,0 +1,17 @@
+package Day50177;
+
+public class Main {
+    static int findCheapestProduct(int[] prices){
+         int cheapest = Integer.MAX_VALUE;
+        for(int price:prices){
+            if(price>100&&price<cheapest){
+                cheapest=price;
+            }
+        }
+          return (cheapest == Integer.MAX_VALUE) ? -1 : cheapest;
+    }
+    public static void main(String[] args) {
+        int[] prices = {80, 150, 120, 95, 200, 110};
+        System.out.println(findCheapestProduct(prices));
+    }
+}
