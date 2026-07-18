b18bc0b3ac33c61445a6007879d40f44c0d5ddd6
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-18 22:11:02 +0530

    18726

--- Day51187/Main.java
+++ Day51187/Main.java
@@ -2,9 +2,9 @@ package Day51187;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Hello World!");
+//     }
+// }
