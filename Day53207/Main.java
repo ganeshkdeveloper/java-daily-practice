b6b2c8e109424160c9d71ae527d94e7040295656
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-20 10:38:06 +0530

    Once again stand alone

--- Day53207/Main.java
+++ Day53207/Main.java
@@ -53,18 +53,18 @@ package Day53207;
 /**
  * Main
  */
-public class Main {
-    static int countConsonants(String name){
-        int count=0;
-        for(int i=0;i<name.length();i++){
-            char ch=Character.toLowerCase(name.charAt(i));
-            if(Character.isLetter(ch) &&ch!='a'&&ch!='e'&&ch!='i'&&ch!='o'&&ch!='u'){
-                count++;
-            }
-        }
-        return count;
-    }
-     public static void main(String[] args) {
-        System.out.println(countConsonants("Ganesh"));
-     }
-}
+// public class Main {
+//     static int countConsonants(String name){
+//         int count=0;
+//         for(int i=0;i<name.length();i++){
+//             char ch=Character.toLowerCase(name.charAt(i));
+//             if(Character.isLetter(ch) &&ch!='a'&&ch!='e'&&ch!='i'&&ch!='o'&&ch!='u'){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+//      public static void main(String[] args) {
+//         System.out.println(countConsonants("Ganesh"));
+//      }
+// }
