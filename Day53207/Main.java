81f07443b20c329ea29753c5070693d3792b2c77
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-20 09:35:47 +0530

    Once again stand alone

--- Day53207/Main.java
+++ Day53207/Main.java
@@ -1,17 +1,17 @@
 package Day53207;
 
-public class Main {
-    static int countVowels(String name){
-        int count=0;
-        for(int i=0;i<name.length();i++){
-            char ch=Character.toLowerCase(name.charAt(i));
-            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
-                count++;
-            }
-        }
-        return count;
-    }
-    public static void main(String[] args) {
-        System.out.println(countVowels("Ganesh"));
-    }
-}
+// public class Main {
+//     static int countVowels(String name){
+//         int count=0;
+//         for(int i=0;i<name.length();i++){
+//             char ch=Character.toLowerCase(name.charAt(i));
+//             if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+//     public static void main(String[] args) {
+//         System.out.println(countVowels("Ganesh"));
+//     }
+// }
