ad46b7e73c14c6bd624c52ec0bdd886ecfdcb508
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-20 09:44:31 +0530

    Once again stand alone

--- Day53207/Main.java
+++ Day53207/Main.java
@@ -19,17 +19,17 @@ package Day53207;
 /**
  * Main
  */
-    public class Main {
-        static char findFirstVowel(String name){
-            for(int i=0;i<name.length();i++){
-                char ch=Character.toLowerCase(name.charAt(i));
-                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
-                    return ch;
-                }
-            }
-            return '#';
-        }
-        public static void main(String[] args) {
-            System.out.println(findFirstVowel("Developer"));
-        }
-}
+//     public class Main {
+//         static char findFirstVowel(String name){
+//             for(int i=0;i<name.length();i++){
+//                 char ch=Character.toLowerCase(name.charAt(i));
+//                 if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+//                     return ch;
+//                 }
+//             }
+//             return '#';
+//         }
+//         public static void main(String[] args) {
+//             System.out.println(findFirstVowel("Developer"));
+//         }
+// }
