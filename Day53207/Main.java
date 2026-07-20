d4326e43bfa2024045c4e81e27e56b34ab4ec2c0
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-20 09:44:16 +0530

    Once again stand alone

--- Day53207/Main.java
+++ Day53207/Main.java
@@ -15,3 +15,21 @@ package Day53207;
 //         System.out.println(countVowels("Ganesh"));
 //     }
 // }
+
+/**
+ * Main
+ */
+    public class Main {
+        static char findFirstVowel(String name){
+            for(int i=0;i<name.length();i++){
+                char ch=Character.toLowerCase(name.charAt(i));
+                if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                    return ch;
+                }
+            }
+            return '#';
+        }
+        public static void main(String[] args) {
+            System.out.println(findFirstVowel("Developer"));
+        }
+}
