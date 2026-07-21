60e70e6a89ef58658a75877cb7c97baeb9a6dbe3
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-21 10:13:59 +0530

    Once again stand alone

--- Day54217/Main.java
+++ Day54217/Main.java
@@ -36,29 +36,29 @@ package Day54217;
 //         System.out.println(isPalindrome("level")); 
 //     }
 // }
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class Main {
+// public class Main {
 
-    static boolean isPalindrome(String str) {
-        String reversed = "";
+//     static boolean isPalindrome(String str) {
+//         String reversed = "";
 
-        // Reverse the string
-        for (int i = str.length() - 1; i >= 0; i--) {
-            reversed += str.charAt(i);
-        }
+//         // Reverse the string
+//         for (int i = str.length() - 1; i >= 0; i--) {
+//             reversed += str.charAt(i);
+//         }
 
-        // Compare original and reversed strings
-        return str.equals(reversed);
-    }
+//         // Compare original and reversed strings
+//         return str.equals(reversed);
+//     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
 
-        String str = sc.nextLine();
+//         String str = sc.nextLine();
 
-        System.out.println(isPalindrome(str));
+//         System.out.println(isPalindrome(str));
 
-        sc.close();
-    }
-}
+//         sc.close();
+//     }
+// }
