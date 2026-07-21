32ddae5b41e67272328b2500a406e026bacc8617
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-21 09:49:38 +0530

    Once again stand alone

--- Day54217/Main.java
+++ Day54217/Main.java
@@ -1,15 +1,15 @@
 package Day54217;
 
-public class Main {
-    static String reverseString(String str){
-        String reversed="";
-        for(int i=str.length()-1;i>=0;i--){
-            reversed+=str.charAt(i);
-        }
-        return reversed;
-    }
-    public static void main(String[] args) {
-        String input="Ganesh";
-        System.out.println(reverseString(input));
-    }
-}
+// public class Main {
+//     static String reverseString(String str){
+//         String reversed="";
+//         for(int i=str.length()-1;i>=0;i--){
+//             reversed+=str.charAt(i);
+//         }
+//         return reversed;
+//     }
+//     public static void main(String[] args) {
+//         String input="Ganesh";
+//         System.out.println(reverseString(input));
+//     }
+// }
