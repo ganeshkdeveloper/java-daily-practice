8dfebb0d69d5b02a18a920a1cf197bbe39bbb01c
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-22 17:19:40 +0530

    Once again stand alone

--- Day55227/Main.java
+++ Day55227/Main.java
@@ -61,9 +61,9 @@ package Day55227;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println(2+2);
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println(2+2);
+//     }
+// }
