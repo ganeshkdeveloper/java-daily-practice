aab7fe12da29ebc66766d438b82d6ad4c11c4924
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-22 17:22:30 +0530

    Once again stand alone

--- Day55227/Main.java
+++ Day55227/Main.java
@@ -97,9 +97,9 @@ package Day55227;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("No One is Coming To Save You");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("No One is Coming To Save You");
+//     }
+// }
