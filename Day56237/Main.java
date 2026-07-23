ad17342efaad8fab873511af767c8c53d342baed
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-23 17:07:38 +0530

    Once again stand alone

--- Day56237/Main.java
+++ Day56237/Main.java
@@ -16,9 +16,9 @@ package Day56237;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Once Again Stand Alone");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Once Again Stand Alone");
+//     }
+// }
