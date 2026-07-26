a890d7abad65b59a72a37a0df79a287d852de5ec
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-26 23:46:01 +0530

    Once again stand alone

--- Day58267/Main.java
+++ Day58267/Main.java
@@ -7,9 +7,9 @@ package Day58267;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Dont be Afraid");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Dont be Afraid");
+//     }
+// }
