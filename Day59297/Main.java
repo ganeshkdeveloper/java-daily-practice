9104de644a68183a951315f79074edff9c74ac9d
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-27 17:27:05 +0530

    Once again stand alone

--- Day59297/Main.java
+++ Day59297/Main.java
@@ -25,9 +25,9 @@ package Day59297;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Neter this worls");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Neter this worls");
+//     }
+// }
