a83d5bd92557c416004ce04c75cd0c5bb6ad10b5
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-27 17:26:38 +0530

    Once again stand alone

--- Day59297/Main.java
+++ Day59297/Main.java
@@ -16,9 +16,9 @@ package Day59297;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World of java");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Hello World of java");
+//     }
+// }
