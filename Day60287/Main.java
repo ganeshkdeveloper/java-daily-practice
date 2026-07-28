5a8005e95ce355916c851e0d933b9e798f3f1e6d
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-28 14:04:46 +0530

    Once again stand alone

--- Day60287/Main.java
+++ Day60287/Main.java
@@ -1,7 +1,7 @@
 package Day60287;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Everything works if u works");
-    }
-}
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Everything works if u works");
+//     }
+// }
