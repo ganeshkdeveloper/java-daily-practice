ae35b91c657b4c65f6c38983523440665d4bd767
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-31 11:21:17 +0530

    Once Again Stand Alone

--- Day63317/Main.java
+++ Day63317/Main.java
@@ -32,3 +32,13 @@ import java.util.*;
 //         sc.close();
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Banking");
+    }
+}
