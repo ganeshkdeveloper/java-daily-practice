6c2aa97dae1ca44f5afb410973e45caad37a3997
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-31 11:21:30 +0530

    Once Again Stand Alone

--- Day63317/Main.java
+++ Day63317/Main.java
@@ -35,10 +35,10 @@ import java.util.*;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello Banking");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Hello World");
+//         System.out.println("Hello Banking");
+//     }
+// }
