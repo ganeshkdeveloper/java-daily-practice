786e95ab956c66b306c32c66936512e65423a240
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-07-31 11:11:36 +0530

    Once Again Stand Alone

--- Day63317/Main.java
+++ Day63317/Main.java
@@ -1,14 +1,14 @@
 package Day63317;
 import java.util.*;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name=sc.nextLine();
-        System.out.println("Enter your age: ");
-        int age=sc.nextInt();
-        System.out.println("Your name is "+name);
-        System.out.println("Your age is "+age);
-        sc.close();
-    }
-}
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter your name: ");
+//         String name=sc.nextLine();
+//         System.out.println("Enter your age: ");
+//         int age=sc.nextInt();
+//         System.out.println("Your name is "+name);
+//         System.out.println("Your age is "+age);
+//         sc.close();
+//     }
+// }
