c4b96bcf0d1760ff6b2a12c49e63129802997f59
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-02 23:45:12 +0530

    Once Again Stand Alone

--- Day64028/Main.java
+++ Day64028/Main.java
@@ -17,3 +17,12 @@ package Day64028;
 //         System.out.println("Hello World");
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
