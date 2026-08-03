ee41f1afc18a055952b8e3b297ea23fe0585fb8e
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-03 09:59:26 +0530

    Once Again Stand Alone

--- Day65038/Main.java
+++ Day65038/Main.java
@@ -54,9 +54,9 @@ package Day65038;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Work Hard Like Hell");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Work Hard Like Hell");
+//     }
+// }
