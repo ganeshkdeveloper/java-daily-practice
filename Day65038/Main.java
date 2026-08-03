88dca942f90ecde60dee8a68d9fcc8889647793a
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-03 12:03:38 +0530

    Once Again Stand Alone

--- Day65038/Main.java
+++ Day65038/Main.java
@@ -60,3 +60,33 @@ package Day65038;
 //         System.out.println("Work Hard Like Hell");
 //     }
 // }
+
+/**
+ * Main
+ */
+public class Main {
+    static int findSecondLargest(int[] arr){
+        if(arr==null||arr.length<2){
+            return -1;
+        }
+        int largest=Integer.MIN_VALUE;
+        int secondLargest=Integer.MIN_VALUE;
+        for(int num:arr){
+            if(num>largest){
+                secondLargest=largest;
+                largest=num;
+            }else if(num>secondLargest&&num!=largest){
+                secondLargest=num;
+            }
+        }
+        return (secondLargest==Integer.MIN_VALUE)?-1:secondLargest;
+    }
+    public static void main(String[] args) {
+        int[] arr1 = {10, 20, 30, 40, 50};
+        int[] arr2 = {5, 5, 5};
+        int[] arr3 = {8, 15, 15, 10};
+        System.out.println(findSecondLargest(arr1));
+        System.out.println(findSecondLargest(arr2));
+        System.out.println(findSecondLargest(arr3));
+    }
+}
