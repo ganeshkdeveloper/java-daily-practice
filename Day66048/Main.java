922106909aec71028b8b1409856e50a019f83368
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-04 09:35:54 +0530

    Once Again Stand Alone

---
+++ Day66048/Main.java
@@ -0,0 +1,29 @@
+package Day66048;
+// public class Main {
+//     static int findFirstUnique(int[] arr){
+//         if(arr==null||arr.length==0){
+//             return -1;
+//         }
+//         for(int i=0;i<arr.length;i++){
+//             int current=arr[i];
+//             int count=0;
+//             for(int j=0;j<arr.length;j++){
+//                 if(arr[j]==current){
+//                     count++;
+//                 }
+//             }
+//             if(count==1){
+//                 return current;
+//             }
+//         }
+//         return -1;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr1={2,3,4,2,3,5,4};
+//         int[] arr2={1,1,2,2};
+//         int[] arr3={7,8,7,9,8};
+//         System.out.println(findFirstUnique(arr1));
+//         System.out.println(findFirstUnique(arr2));
+//         System.out.println(findFirstUnique(arr3));
+//     }
+// }
