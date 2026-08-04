d0df416f87232ff09fff5cb21507b444bb59e135
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-04 09:46:03 +0530

    Once Again Stand Alone

--- Day66048/Main.java
+++ Day66048/Main.java
@@ -59,3 +59,17 @@ package Day66048;
 //         }
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int num=-5;
+        if(num>0){
+            System.out.println("Positive");
+        }else{
+            System.out.println("Negative");
+        }
+    }
+}
