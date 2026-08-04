ef7248cd96614a1e5eba342fe4fbe5c4eaf41279
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-04 09:45:18 +0530

    Once Again Stand Alone

--- Day66048/Main.java
+++ Day66048/Main.java
@@ -48,14 +48,14 @@ package Day66048;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        int num=7;
-        if(num%2==0){
-            System.out.println("Even");
-        }else{
-            System.out.println("odd");
-        }
-    }
-}
+//     public static void main(String[] args) {
+//         int num=7;
+//         if(num%2==0){
+//             System.out.println("Even");
+//         }else{
+//             System.out.println("odd");
+//         }
+//     }
+// }
