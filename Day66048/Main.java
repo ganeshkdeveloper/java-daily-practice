705f643540379d29584b6330af46e0263512f385
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-04 09:43:33 +0530

    Once Again Stand Alone

--- Day66048/Main.java
+++ Day66048/Main.java
@@ -30,9 +30,18 @@ package Day66048;
 /**
  * Main
  */
+// public class Main {
+
+//     public static void main(String[] args) {
+//         System.out.println("Hello World");
+//     }
+// }
+/**
+ * Main
+ */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(10+20);
     }
 }
