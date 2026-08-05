c04db7bd730b9bcf52b407ce9c115eafa862b294
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-05 09:39:26 +0530

    Once Again Stand Alone

---
+++ Day67058/Main.java
@@ -0,0 +1,22 @@
+package Day67058;
+public class Main {
+    static int reverseNumber(int num){
+        int reversed=0;
+        while (num>0) {
+            int digit=num%10;
+            reversed=reversed*10+digit;
+            num=num/10;
+        }
+        return reversed;
+    }
+    public static void main(String[] args) {
+        int num1=12345;
+        int num2=1002;
+        int num3=7;
+        System.out.println(reverseNumber(num1));
+        System.out.println(reverseNumber(num2));
+        System.out.println(reverseNumber(num3));
+
+    }
+}
+
