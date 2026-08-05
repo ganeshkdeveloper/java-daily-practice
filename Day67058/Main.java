ada0026124e867acf20d2f87ca45134193216cf7
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-05 09:47:05 +0530

    Once Again Stand Alone

--- Day67058/Main.java
+++ Day67058/Main.java
@@ -22,25 +22,25 @@ package Day67058;
 /**
  * Main
  */
-public class Main {
-    static boolean isPalindrome(int num){
-        int original=num;
-        int reversed=0;
-        while (num>0) {
-            int digit=num%10;
-            reversed=reversed*10+digit;
-            num=num/10;
-        }
-        return original==reversed;
-    }
-    public static void main(String[] args) {
-        int num1=121;
-        int num2 = 123;
-        int num3 = 1221;
-        int num4 = 7;
-        System.out.println(isPalindrome(num1));
-        System.out.println(isPalindrome(num2));
-        System.out.println(isPalindrome(num3));
-        System.out.println(isPalindrome(num4));
-    }
-}
+// public class Main {
+//     static boolean isPalindrome(int num){
+//         int original=num;
+//         int reversed=0;
+//         while (num>0) {
+//             int digit=num%10;
+//             reversed=reversed*10+digit;
+//             num=num/10;
+//         }
+//         return original==reversed;
+//     }
+//     public static void main(String[] args) {
+//         int num1=121;
+//         int num2 = 123;
+//         int num3 = 1221;
+//         int num4 = 7;
+//         System.out.println(isPalindrome(num1));
+//         System.out.println(isPalindrome(num2));
+//         System.out.println(isPalindrome(num3));
+//         System.out.println(isPalindrome(num4));
+//     }
+// }
