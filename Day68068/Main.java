f9b917d53c8673540d93bda87d0d460bfc2e2461
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-06 09:35:43 +0530

    Once Again Stand Alone

--- Day68068/Main.java
+++ Day68068/Main.java
@@ -22,3 +22,29 @@ package Day68068;
 //         System.out.println(reverseNumber(num3));
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+    static boolean isPalindrome(int num){
+        int original=num;
+        int reversed=0;
+        while (num>0) {
+
+            int digit=num%10;
+            reversed=reversed*10+digit;
+            num=num/10;
+        }
+        return reversed==original;
+    }
+    public static void main(String[] args) {
+        int num1=121;
+        int num2=123;
+        int num3=1221;
+        int num4=7;
+        System.out.println(isPalindrome(num1));
+        System.out.println(isPalindrome(num2));
+        System.out.println(isPalindrome(num3));
+        System.out.println(isPalindrome(num4));
+    }
+}
