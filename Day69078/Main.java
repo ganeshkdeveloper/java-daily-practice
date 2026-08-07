2e99cfb4886152baa0501c6ae5256a3615b46f06
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-07 14:20:46 +0530

    Once Again Stand Alone

--- Day69078/Main.java
+++ Day69078/Main.java
@@ -1,12 +1,12 @@
 package Day69078;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Hai! Enter Your salary wish!: ");
-        int num=sc.nextInt();
-        System.out.println("Oh! your wish is to earn "+num+","+ "congragulations!");
-    }
-}
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Hai! Enter Your salary wish!: ");
+//         int num=sc.nextInt();
+//         System.out.println("Oh! your wish is to earn "+num+","+ "congragulations!");
+//     }
+// }
