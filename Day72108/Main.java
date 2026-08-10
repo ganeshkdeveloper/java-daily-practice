844b802403823fd0612215570ab5cff17557fdef
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-10 10:07:45 +0530

    Once Again Stand Alone

--- Day72108/Main.java
+++ Day72108/Main.java
@@ -7,21 +7,21 @@ package Day72108;
 /**
  * Main
  */
-public class Main {
-    static void swapNumbers(int a,int b){
-        System.out.println("Before:");
-        System.out.println("a = "+a);
-        System.out.println("b = "+b);
-        int temp=a;
-        a=b;
-        b=temp;
-        System.out.println("After:");
-        System.out.println("a = "+a);
-        System.out.println("b = "+b);
-    }
-    public static void main(String[] args) {
-        int a=10;
-        int b=20;
-        swapNumbers(a, b);
-    }
-}
+// public class Main {
+//     static void swapNumbers(int a,int b){
+//         System.out.println("Before:");
+//         System.out.println("a = "+a);
+//         System.out.println("b = "+b);
+//         int temp=a;
+//         a=b;
+//         b=temp;
+//         System.out.println("After:");
+//         System.out.println("a = "+a);
+//         System.out.println("b = "+b);
+//     }
+//     public static void main(String[] args) {
+//         int a=10;
+//         int b=20;
+//         swapNumbers(a, b);
+//     }
+// }
