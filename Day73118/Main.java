7ac0bf34906f36856fbd774d93a02ce245c6a7b1
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-11 09:52:40 +0530

    Once Again Stand Alone

--- Day73118/Main.java
+++ Day73118/Main.java
@@ -5,3 +5,20 @@ package Day73118;
 //         System.out.println("Start Java");
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+    static int findLargest(int a, int b){
+        if(a>b){
+            return a;
+        }else{
+            return b;
+        }
+    }
+    public static void main(String[] args) {
+         System.out.println(findLargest(10, 20));
+        System.out.println(findLargest(50, 30));
+        System.out.println(findLargest(7, 7));
+    }
+}
