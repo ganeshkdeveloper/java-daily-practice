661bb9c4aa232da4fb93e9c057f6c2069186bbc8
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-11 09:55:56 +0530

    Once Again Stand Alone

--- Day73118/Main.java
+++ Day73118/Main.java
@@ -43,9 +43,9 @@ package Day73118;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Close today");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Close today");
+//     }
+// }
