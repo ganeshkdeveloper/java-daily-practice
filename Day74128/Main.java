26a98f9471cf29ec6a1c3dccd4b825a269b65d33
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-12 10:08:45 +0530

    Once Again Stand Alone

--- Day74128/Main.java
+++ Day74128/Main.java
@@ -22,3 +22,12 @@ package Day74128;
 //         System.out.println("Print java");
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
