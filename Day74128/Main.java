29a150086d40544ab1474318caf9b9888555c6a1
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-12 10:06:42 +0530

    Once Again Stand Alone

--- Day74128/Main.java
+++ Day74128/Main.java
@@ -1,15 +1,15 @@
 package Day74128;
 
-public class Main {
-    static int findSmallest(int a,int b){
-        if(a<b){
-            return a;
-        }else{
-            return b;
-        }
-    }
-    public static void main(String[] args) {
-         System.out.println(findSmallest(10, 20));
-        System.out.println(findSmallest(50, 30)); 
-        System.out.println(findSmallest(7, 7));   
-}}
+// public class Main {
+//     static int findSmallest(int a,int b){
+//         if(a<b){
+//             return a;
+//         }else{
+//             return b;
+//         }
+//     }
+//     public static void main(String[] args) {
+//          System.out.println(findSmallest(10, 20));
+//         System.out.println(findSmallest(50, 30)); 
+//         System.out.println(findSmallest(7, 7));   
+// }}
