011bab4f04fc10a0a726754cb9edc5f375d0c770
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-13 12:04:00 +0530

    Once Again Stand Alone

--- Day75138/Main.java
+++ Day75138/Main.java
@@ -1,11 +1,11 @@
 package Day75138;
-public class Main {
-    static int addNumbers(int a,int b){
-        return a+b;
-    }
-    public static void main(String[] args) {
-          System.out.println(addNumbers(10, 20)); 
-        System.out.println(addNumbers(50, 30)); 
-        System.out.println(addNumbers(7, 7));    
-    }
-}
+// public class Main {
+//     static int addNumbers(int a,int b){
+//         return a+b;
+//     }
+//     public static void main(String[] args) {
+//           System.out.println(addNumbers(10, 20)); 
+//         System.out.println(addNumbers(50, 30)); 
+//         System.out.println(addNumbers(7, 7));    
+//     }
+// }
