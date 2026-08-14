a59f44724906e807160ed6d392d03b2213ecfcb7
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-14 11:29:51 +0530

    Once Again Stand Alone

--- Day76148/Main.java
+++ Day76148/Main.java
@@ -40,11 +40,11 @@ package Day76148;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello java");
-        System.out.println("Hello py");
-        System.out.println("Hello Runtime");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Hello java");
+//         System.out.println("Hello py");
+//         System.out.println("Hello Runtime");
+//     }
+// }
