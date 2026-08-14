4fa50cc7fd3f03d56a1cfa392bcee4259e3e7ce3
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-14 11:27:18 +0530

    Once Again Stand Alone

--- Day76148/Main.java
+++ Day76148/Main.java
@@ -2,15 +2,29 @@ package Day76148;
 /**
  * Main
  */
+// public class Main {
+//     static boolean isEven(int num){
+//         return num%2==0;
+//     }
+//     public static void main(String[] args) {
+//         System.out.println(isEven(10));  
+//         System.out.println(isEven(7));   
+//         System.out.println(isEven(24));  
+//         System.out.println(isEven(15));  
+//         System.out.println(isEven(0)); 
+//     }
+// }
+/**
+ * Main
+ */
+import java.util.*;
 public class Main {
-    static boolean isEven(int num){
-        return num%2==0;
-    }
+
     public static void main(String[] args) {
-        System.out.println(isEven(10));  
-        System.out.println(isEven(7));   
-        System.out.println(isEven(24));  
-        System.out.println(isEven(15));  
-        System.out.println(isEven(0)); 
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name=sc.nextLine();
+        System.out.println("Your name is: "+name);
+        sc.close();
     }
 }
