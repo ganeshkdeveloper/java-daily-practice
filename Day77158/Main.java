f6172a0e67812430e082b3a67dbece60306c23ee
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-15 22:06:36 +0530

    Once Again Stand Alone

--- Day77158/Main.java
+++ Day77158/Main.java
@@ -1,7 +1,7 @@
 package Day77158;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Heelo World");
-    }
-}
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Heelo World");
+//     }
+// }
