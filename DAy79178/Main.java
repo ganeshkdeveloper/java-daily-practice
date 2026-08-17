e8efe9e49fc6f8a29b28f836f7948ec1f9bf4c13
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-17 10:01:19 +0530

    Once Again Stand Alone

--- DAy79178/Main.java
+++ DAy79178/Main.java
@@ -44,9 +44,9 @@ package DAy79178;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Login welcome");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Login welcome");
+//     }
+// }
