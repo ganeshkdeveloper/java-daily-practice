280d5f3db970148feddde725188b0c2b5e2839cd
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-18 10:18:59 +0530

    Once Again Stand Alone

--- Day80188/Main.java
+++ Day80188/Main.java
@@ -16,13 +16,13 @@ import java.util.Scanner;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter your name:");
-        String name=sc.nextLine();
-        System.out.println("your name is "+name);
-        sc.close();
-    }
-}
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter your name:");
+//         String name=sc.nextLine();
+//         System.out.println("your name is "+name);
+//         sc.close();
+//     }
+// }
