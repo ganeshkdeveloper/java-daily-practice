0d3f6371b2183f9581e08eef2dd0a4bd701e829e
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-18 10:15:24 +0530

    Once Again Stand Alone

--- Day80188/Main.java
+++ Day80188/Main.java
@@ -1,4 +1,12 @@
 package Day80188;
 public class Main {
-    
+    static int square(int num){
+    return num*num;
+    }
+    public static void main(String[] args) {
+         System.out.println(square(5));   
+        System.out.println(square(10));  
+        System.out.println(square(7));   
+        System.out.println(square(0)); 
+    }
 }
