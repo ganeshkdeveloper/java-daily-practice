f287e605cc11d7f32c1501e66ff506135a31deb1
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-21 09:21:26 +0530

    Once Again Stand Alone

--- Day82218/Main.java
+++ Day82218/Main.java
@@ -1,7 +1,7 @@
 package Day82218;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Class method");
-    }
-}
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Class method");
+//     }
+// }
