1051ecdc0865f55eff2f3cc84a4250d2aade04e3
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-22 11:57:19 +0530

    Once Again Stand Alone

--- Day83228/Main.java
+++ Day83228/Main.java
@@ -1,6 +1,15 @@
 package Day83228;
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello world");
+//     }
+// }
+/**
+ * Main
+ */
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello java and python");
     }
 }
