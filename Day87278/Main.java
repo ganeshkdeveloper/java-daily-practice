a84a9017b8c2fecfe2184e5da8f985270b26d30f
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-27 12:41:55 +0530

    Once Again Stand Alone

--- Day87278/Main.java
+++ Day87278/Main.java
@@ -70,9 +70,9 @@ package Day87278;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("git add .");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("git add .");
+//     }
+// }
