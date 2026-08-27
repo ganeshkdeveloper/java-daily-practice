dbda275b39aba43934ccbafe20703364bdff6f68
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-08-27 12:43:23 +0530

    Once Again Stand Alone

--- Day87278/Main.java
+++ Day87278/Main.java
@@ -85,3 +85,12 @@ package Day87278;
 //         System.out.println("hello");
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
