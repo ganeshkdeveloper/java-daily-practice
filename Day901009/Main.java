e22b7a3773d721334e3269d34a4a938e4d95e89c
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-10 09:35:35 +0530

    Once Again Stand Alone

--- Day901009/Main.java
+++ Day901009/Main.java
@@ -14,3 +14,12 @@ package Day901009;
 //         System.out.println("Hello py");
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
