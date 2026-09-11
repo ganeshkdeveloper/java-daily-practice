bcf2cecae933853cd39f5328159a7204e70001be
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-11 09:36:15 +0530

    Once Again Stand Alone

--- Day911109/Main.java
+++ Day911109/Main.java
@@ -73,9 +73,9 @@ package Day911109;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Push harder");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Push harder");
+//     }
+// }
