dede7b96af6765304779fd2654635f4074da3123
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-15 09:49:04 +0530

    Once Again Stand Alone

--- Day921509/Main.java
+++ Day921509/Main.java
@@ -13,3 +13,12 @@ package Day921509;
 //         System.out.println("hi java");
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
