5c75e323e1cf7e9d98f8b8507c3a6d0c3523b6f2
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-16 09:46:25 +0530

    Once Again Stand Alone

--- Day931609/Main.java
+++ Day931609/Main.java
@@ -1,12 +1,12 @@
 package Day931609;
-public class Main {
-    public static void main(String[] args) {
-        int[] numbers={5, 15, 25, 35, 45};
-        int search=25;
-        for(int i=0;i<numbers.length;i++){
-            if(numbers[i]==search){
-                 System.out.println("Found");
-            }
-        }
-    }
-}
+// public class Main {
+//     public static void main(String[] args) {
+//         int[] numbers={5, 15, 25, 35, 45};
+//         int search=25;
+//         for(int i=0;i<numbers.length;i++){
+//             if(numbers[i]==search){
+//                  System.out.println("Found");
+//             }
+//         }
+//     }
+// }
