fbdaec85646f2aea38bc3d93815c27bac24e301c
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-16 10:20:32 +0530

    Once Again Stand Alone

--- Day931609/Main.java
+++ Day931609/Main.java
@@ -55,3 +55,14 @@ package Day931609;
 //         greet("Run");
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+    static void printAge(int age){
+        System.out.println("Age: "+age);
+    }
+    public static void main(String[] args) {
+        printAge(26);
+    }
+}
