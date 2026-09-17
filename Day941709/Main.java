bafcff8e8eaf80ee88bdf7e5cec26b23d9d93ee5
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-17 09:50:33 +0530

    Once Again Stand Alonee

--- Day941709/Main.java
+++ Day941709/Main.java
@@ -12,3 +12,16 @@ package Day941709;
 //         System.out.println(add(10,20,30));
 //     }
 // }
+
+public class Main{
+    static int multiply(int a, int b){
+        return a*b;
+    }
+    static int multiply(int a, int b, int c){
+        return a*b*c;
+    }
+    public static void main(String[] args){
+        System.out.println(multiply(2, 3));
+          System.out.println(multiply(2, 3,4));
+    }
+}
