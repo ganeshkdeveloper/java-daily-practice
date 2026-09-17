797b3f04c30085cedda45fa74ebf3a86f585978a
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-17 09:48:01 +0530

    Once Again Stand Alonee

--- Day941709/Main.java
+++ Day941709/Main.java
@@ -1,14 +1,14 @@
 package Day941709;
 
-public class Main {
-    static int add(int a, int b){
-        return a+b;
-    }
-    static int add(int a,int b,int c){
-        return a+b+c;
-    }
-    public static void main(String[] args){
-        System.out.println(add(10,20));
-        System.out.println(add(10,20,30));
-    }
-}
+// public class Main {
+//     static int add(int a, int b){
+//         return a+b;
+//     }
+//     static int add(int a,int b,int c){
+//         return a+b+c;
+//     }
+//     public static void main(String[] args){
+//         System.out.println(add(10,20));
+//         System.out.println(add(10,20,30));
+//     }
+// }
