7456625e63a3e14eaed2664752f8f81052efd9f7
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-22 11:28:45 +0530

    Once Again Stand Alonee

---
+++ Day972209/Main.java
@@ -0,0 +1,11 @@
+package Day972209;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int a=sc.nextInt();
+        System.out.println("U entered: "+a);
+        sc.close();
+    }
+}
