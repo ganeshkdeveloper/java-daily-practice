ca8cba14da2714945f16fec02809b56a54f55dee
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-22 11:35:53 +0530

    Once Again Stand Alonee

--- Day972209/Main.java
+++ Day972209/Main.java
@@ -48,9 +48,9 @@ package Day972209;
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello python");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Hello python");
+//     }
+// }
