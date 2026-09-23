b9f68952e307390dec849ea97868d0e319734c57
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-23 15:15:05 +0530

    Once Again Stand Alonee

--- Day982309/Main.java
+++ Day982309/Main.java
@@ -637,3 +637,14 @@
 //         System.out.println(Math.abs(number));
 //     }
 // }
+/**
+ * Main
+ */
+// public class Main {
+
+//     public static void main(String[] args) {
+//         //Math formula
+//         double result = Math.pow(3, 4);
+//         System.out.println(result);
+//     }
+// }
