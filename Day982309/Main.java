c6ceba7054cdf384efcc7a94b9360e3a3cf8e950
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-23 09:58:47 +0530

    Once Again Stand Alone

--- Day982309/Main.java
+++ Day982309/Main.java
@@ -61,9 +61,9 @@
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello java and python");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Hello java and python");
+//     }
+// }
