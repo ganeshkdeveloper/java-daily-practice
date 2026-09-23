082aa07ab51a4e5515fa762d723e3be1093d954e
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-23 11:40:54 +0530

    Once Again Stand Alone

--- Day982309/Main.java
+++ Day982309/Main.java
@@ -283,3 +283,18 @@
     //         System.out.println(!hasLicense);
     //     }
     // }
+    /**
+     * Main
+     */
+    // public class Main {
+    
+    //     public static void main(String[] args) {
+    //         int number = 10;
+    //         System.out.println(number++);
+    //         System.out.println(number);
+    //         System.out.println(++number);
+    //         System.out.println(number--);
+    //         System.out.println(number);
+
+    //     }
+    // }
