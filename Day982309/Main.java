4bed27f65953ce65354d8a553b693b6e2740a43a
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-23 15:42:44 +0530

    Once Again Stand Alonee

--- Day982309/Main.java
+++ Day982309/Main.java
@@ -724,3 +724,15 @@
 //         System.out.println(isAdult);
 //     }
 // }
+/**
+ * Main
+ */
+// public class Main {
+
+//     public static void main(String[] args) {
+//         int age=26;
+//         boolean hasLicense = true;
+//         boolean canDrive = age>=18 && hasLicense;
+//         System.out.println(canDrive);
+//     }
+// }
