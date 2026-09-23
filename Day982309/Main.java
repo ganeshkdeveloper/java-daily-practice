5728c95a23fe2afd58d56a51c82c4faeba12a47f
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-23 09:56:43 +0530

    Once Again Stand Alone

--- Day982309/Main.java
+++ Day982309/Main.java
@@ -21,3 +21,12 @@
 //         System.out.println("Helloworld");
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello junior");
+    }
+}
