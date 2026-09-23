890cb1c8d2f14a022e5c8d6ec44e13525108f5c0
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-23 10:42:01 +0530

    Once Again Stand Alonee

--- Day982309/Main.java
+++ Day982309/Main.java
@@ -169,3 +169,23 @@
     //        System.out.println("Goal: "+ goal);
     //     }
     // }
+    /**
+     * Main
+     */
+    public class Main {
+    
+        public static void main(String[] args) {
+            // Personal Information
+            String name = "Ganesh";
+            int age = 26;
+            String city = "Chennai";
+            String job = "Software Engineer";
+            int experience = 1;
+            // Personal Information outputs
+            System.out.println("My name is "+name);
+            System.out.println("I am "+age+" years old"); 
+            System.out.println("I live in "+city);
+            System.out.println("I work as a "+ job);
+            System.out.println("I have "+experience+" years of experience");
+        }
+    }
