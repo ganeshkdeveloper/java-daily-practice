263545c078ee1a5cf52a6ea5a7a234cba5f55dda
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-23 15:08:34 +0530

    Once Again Stand Alonee

--- Day982309/Main.java
+++ Day982309/Main.java
@@ -606,3 +606,14 @@
 //         System.out.println(Math.max(a, b));
 //     }
 //   }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int a=75;
+        int b=120;
+        System.out.println(Math.min(a,b));
+    }
+}
