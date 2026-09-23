40fe123fe8ec04638be8df8ed8fe28e7379679ec
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-23 14:01:10 +0530

    Once Again Stand Alone

--- Day982309/Main.java
+++ Day982309/Main.java
@@ -555,3 +555,15 @@
     //         System.out.println(role);
     //     }
     // }
+    /**
+     * Main
+     */
+    public class Main {
+    
+        public static void main(String[] args) {
+            String city1 = new String("Chennai");
+            String city2 = new String("Chennai");
+            System.out.println(city1.equals(city2));
+            System.out.println(city1==city2);
+        }
+    }
