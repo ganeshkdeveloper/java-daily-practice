c9973eb70ba77542babc5ceca53136dfcd04237f
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-23 09:59:36 +0530

    Once Again Stand Alone

--- Day982309/Main.java
+++ Day982309/Main.java
@@ -70,10 +70,10 @@
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Println");
-        System.out.println("Println");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Println");
+//         System.out.println("Println");
+//     }
+// }
