f6b64c2b13480bd9d3db7f2c221fb226d244e208
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-24 09:27:36 +0530

    Once Again Stand Alone

--- Day992409/Main.java
+++ Day992409/Main.java
@@ -1,12 +1,12 @@
 package Day992409;
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        //if condition
-        int age = 26;
-        if(age>=18){
-            System.out.println("You can vote");
-        }
+//     public static void main(String[] args) {
+//         //if condition
+//         int age = 26;
+//         if(age>=18){
+//             System.out.println("You can vote");
+//         }
 
-    }
-}
+//     }
+// }
