26baab05ef953c21b42f47c4e4c0a7bf285e914d
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-24 10:36:26 +0530

    Once Again Stand Alonee

--- Day992409/Main.java
+++ Day992409/Main.java
@@ -150,3 +150,27 @@
 //         }
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int day = 3;
+        switch (day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Weekday");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Weekend");
+                break;
+            default:
+                System.out.println("Invalid day");
+            }
+    }
+}
