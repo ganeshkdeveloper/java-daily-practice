8764c2ba4bbd986a9a52a5c14c8a23a3b1729bd9
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-24 13:56:17 +0530

    Once Again Stand Alone

--- Day992409/Main.java
+++ Day992409/Main.java
@@ -334,3 +334,18 @@
 //         System.out.println(sum);
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i<=50) {
+            if(i%5==0){
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
