714b2b295884f7141385e35b3fc6d31b055903c4
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-24 10:02:01 +0530

    Once Again Stand Alonee

--- Day992409/Main.java
+++ Day992409/Main.java
@@ -60,3 +60,21 @@
 //         }
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 65;
+        if(age>=60){
+            System.out.println("Senoir Citizen");
+        }else if(age>=30){
+            System.out.println("Adult");
+        }else if(age>=18){
+            System.out.println("Young Adult");
+        }else{
+            System.out.println("Minor");
+        }
+    }
+}
