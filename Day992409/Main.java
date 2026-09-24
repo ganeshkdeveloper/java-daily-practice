32cd7ef90ada5524792c2bc1869c132bd0416d23
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-24 09:51:22 +0530

    Once Again Stand Alone

--- Day992409/Main.java
+++ Day992409/Main.java
@@ -9,3 +9,24 @@
 //         }
 //     }
 // }
+/**
+ * Main
+ */
+// public class Main {
+
+//     public static void main(String[] args) {
+//         //else-if condition practice
+//         int marks = 68;
+//         if(marks>=90){
+//             System.out.println("Excellent");
+//         }else if(marks>=75){
+//             System.out.println("Very Good");
+//         }else if(marks>=50){
+//             System.out.println("Good");
+//         }else if(marks>=40){
+//             System.out.println("Pass");
+//         }else{
+//             System.out.println("Fail");
+//         }
+//     }
+// }
