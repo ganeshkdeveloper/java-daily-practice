60f9c72119d9e8892eb0a26e9541fd111c3bcc75
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-24 14:11:30 +0530

    Once Again Stand Alone

--- Day992409/Main.java
+++ Day992409/Main.java
@@ -352,18 +352,18 @@
     /**
      * Main
      */
-    public class Main {
+    // public class Main {
     
-        public static void main(String[] args) {
-            int i=1;
-            while (i<=3) {
-                int j=1;
-                while (j<=3) {
-                    System.out.print(j+" ");
-                    j++;
-                }
-                System.out.println();
-                i++;
-            }
-        }
-    }
+    //     public static void main(String[] args) {
+    //         int i=1;
+    //         while (i<=3) {
+    //             int j=1;
+    //             while (j<=3) {
+    //                 System.out.print(j+" ");
+    //                 j++;
+    //             }
+    //             System.out.println();
+    //             i++;
+    //         }
+    //     }
+    // }
