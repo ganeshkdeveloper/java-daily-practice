eaa10035c84becd543df284d285187da5b6a87d2
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-25 09:33:23 +0530

    Once Again Stand Alone

--- Day1002509/Main.java
+++ Day1002509/Main.java
@@ -17,11 +17,11 @@
 /**
  * Main
  */
-public class Main {
-public static void main(String[] args) {
-    for(int i=20; i>=1; i--){
-        System.out.println(i);
-    }
-}
+// public class Main {
+// public static void main(String[] args) {
+//     for(int i=20; i>=1; i--){
+//         System.out.println(i);
+//     }
+// }
     
-}
+// }
