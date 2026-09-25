59086a46951d793ab153c7416f1819fa038b77c6
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-25 11:22:03 +0530

    Once Again Stand Alonee

--- Day1002509/Main.java
+++ Day1002509/Main.java
@@ -180,16 +180,16 @@
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        int[] arr={12, 45, 7, 89, 34, 56};
-        int max = arr[0];
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>max){
-                max = arr[i];
-            }
-        }
-        System.out.println("Largest: "+max);
-    }
-}
+//     public static void main(String[] args) {
+//         int[] arr={12, 45, 7, 89, 34, 56};
+//         int max = arr[0];
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]>max){
+//                 max = arr[i];
+//             }
+//         }
+//         System.out.println("Largest: "+max);
+//     }
+// }
