d76c772ba1b7c648cb403e5a33a1099d2a06d408
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-25 16:02:09 +0530

    Once Again Stand Alonee

--- Day1002509/Main.java
+++ Day1002509/Main.java
@@ -356,3 +356,22 @@
 //         System.out.println(index);
 //     }
 // }
+/**
+ * Main
+ */
+// public class Main {
+
+//     public static void main(String[] args) {
+//         int[][] numbers={
+//             {10,20},
+//             {30,40},
+//             {50,60}
+//         };
+//         for(int i=0;i<numbers.length;i++){
+//             for (int j=0;j<numbers[i].length; j++){
+//                 System.out.print(numbers[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
