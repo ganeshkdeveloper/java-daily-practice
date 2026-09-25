59861585fdfc979c9823583208efb72b209b2da5
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-25 12:05:56 +0530

    Once Again Stand Alonee

--- Day1002509/Main.java
+++ Day1002509/Main.java
@@ -280,3 +280,15 @@
 //         System.out.println(Arrays.toString(arr));
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {5, 10, 15, 20, 25};
+        for(int number:numbers){
+            System.out.println(number);
+        }
+    }
+}
