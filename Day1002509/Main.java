74f2ed28dc5e875032553ae3e5af73630c78f8d3
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-25 11:21:39 +0530

    Once Again Stand Alonee

--- Day1002509/Main.java
+++ Day1002509/Main.java
@@ -177,3 +177,19 @@
 //         System.out.println("Sum: "+sum);
 //     }
 // }
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr={12, 45, 7, 89, 34, 56};
+        int max = arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        System.out.println("Largest: "+max);
+    }
+}
