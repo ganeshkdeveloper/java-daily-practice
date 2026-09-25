7e31f3eba85b0410699b64e2e47a42f40f6f4ef6
ganeshkdeveloper <ganeshkdeveloper2025@gmail.com>
2026-09-25 11:10:28 +0530

    Once Again Stand Alone

--- Day1002509/Main.java
+++ Day1002509/Main.java
@@ -120,12 +120,12 @@
 /**
  * Main
  */
-public class Main {
+// public class Main {
 
-    public static void main(String[] args) {
-        int[] arr={10, 20, 30, 40, 50};
-        System.out.println(arr[0]);
-        System.out.println(arr[4]);
+//     public static void main(String[] args) {
+//         int[] arr={10, 20, 30, 40, 50};
+//         System.out.println(arr[0]);
+//         System.out.println(arr[4]);
 
-    }
-}
+//     }
+// }
